bcd5c31acfe4c75f191d46c425df12b16ee6e746
Rémy El Sibaïe <remy@nomadic-labs.com>
2021-05-17 10:16:18 +0200

    fixup! Tests/Python: add tests for `transfer --simulation`

--- tests_python/tests_alpha/test_client.py
+++ tests_python/tests_alpha/test_client.py
@@ -3,23 +3,35 @@ from client.client import Client
 from tools.utils import assert_run_failure, bake
 
 
+@pytest.fixture(scope="class")
+def encrypted_account(client: Client):
+    """
+    Import an encrypted account
+    """
+    client.import_secret_key(
+        "bootstrap_encrypted"
+        , ("encrypted:edesk1n2uGpPtVaeyhWkZzTEcaPRzkQHrqkw5pk8VkZv"
+        "p3rM5KSc3mYNH5cJEuNcfB91B3G3JakKzfLQSmrgF4ht")
+        , "password"
+        )
+
 @pytest.mark.client
 @pytest.mark.incremental
 class TestSimulation:
     """ Tests the behavior of the --simulation flag. """
 
-    def test_transfer_simulation(self, client: Client):
+    def test_transfer_simulation(self, client: Client, encrypted_account):
         """ Tests that --simulation does not ask for the key password. """
         client.transfer(
-            0.1, 'bootstrap_encrypted', "bootstrap1", ["--simulation"]
+            0.1, "bootstrap_encrypted", "bootstrap1", ["--simulation"]
         )
 
-    def test_transfer_without_simulation(self, client: Client):
+    def test_transfer_without_simulation(self, client: Client, encrypted_account):
         """ Tests that the client asks for the password w/o --simulation. """
         with assert_run_failure('End_of_file', mode="stdout"):
             client.transfer(0.1, 'bootstrap_encrypted', "bootstrap1")
 
-    def test_delegate_simulation(self, client: Client):
+    def test_delegate_simulation(self, client: Client, encrypted_account):
         """ Tests that --simulation does not ask for the key password. """
         client.gen_key('delegate')
         client.transfer(100, 'bootstrap1', 'delegate', ['--burn-cap', '1.0'])
