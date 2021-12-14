79623d3ce33af0f19e50e035b65b747945d04172
Hans Hoglund <hans.hoglund@trili.tech>
2021-12-14 15:37:19 +0000

    FIXUP formatting

--- src/proto_alpha/lib_protocol/test/helpers/block.ml
+++ src/proto_alpha/lib_protocol/test/helpers/block.ml
@@ -686,10 +686,8 @@ let bake_n_with_all_balance_updates ?(baking_mode = Application) ?policy
             fun (Successful_manager_result r) ->
               match r with
               | Reveal_result _ | Delegation_result _
-              | Set_deposits_limit_result _
-              | Tx_rollup_origination_result _
-              | Sc_rollup_originate_result _
-              | Sc_rollup_add_message_result _ ->
+              | Set_deposits_limit_result _ | Tx_rollup_origination_result _
+              | Sc_rollup_originate_result _ | Sc_rollup_add_message_result _ ->
                   balance_updates_rev
               | Transaction_result {balance_updates; _}
               | Origination_result {balance_updates; _}
