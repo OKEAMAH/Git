b9f2353b8729a6a64a765627eb88a7819674bf46
Hans Hoglund <hans.hoglund@trili.tech>
2022-01-06 13:01:03 +0000

    REVERTME: Print storage descr on module init time
    E.g. to see this use dune build src/proto_alpha/lib_protocol

--- src/proto_alpha/lib_protocol/services_registration.ml
+++ src/proto_alpha/lib_protocol/services_registration.ml
@@ -107,6 +107,11 @@ let opt_register2 ~chunked s f =
       f context a1 a2 q i)
 
 let get_rpc_services () =
+  let desc = Alpha_context.description in
+  let desc_str =
+    Format.asprintf "%a" Storage_description.pp desc in
+  let () =
+    Hack.printf "\n\n!!!!!! Generating service desc: \n%s\n\n" desc_str in
   let p =
     RPC_directory.map
       (fun c ->
@@ -114,7 +119,7 @@ let get_rpc_services () =
         | Error t ->
             raise (Failure (Format.asprintf "%a" Error_monad.pp_trace t))
         | Ok c -> c.context)
-      (Storage_description.build_directory Alpha_context.description) in
+      (Storage_description.build_directory desc) in
   let () =
     Logging.(log_string Notice "LOG LOG")
     (*
