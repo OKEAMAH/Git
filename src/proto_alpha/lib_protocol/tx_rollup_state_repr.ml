42336cdb22151eda06eaa28a8b649e547641cb50
Thomas Letan <lthms@nomadic-labs.com>
2022-01-14 17:45:24 +0100

    Proto,tx_rollup: Implement the fees logic for inboxes
    Co-authored-by: Marigold <contact@marigold.dev>
    Co-authored-by: Nomadic Labs <contact@nomadic-labs.com>
    Co-authored-by: Oxhead Alpha <info@oxheadalpha.com>

--- src/proto_alpha/lib_protocol/tx_rollup_state_repr.ml
+++ src/proto_alpha/lib_protocol/tx_rollup_state_repr.ml
@@ -25,10 +25,55 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-type t = unit
+type t = {fees_per_byte : Tez_repr.t}
 
-let initial_state = ()
+let initial_state = {fees_per_byte = Tez_repr.zero}
 
-let encoding = Data_encoding.unit
+let encoding : t Data_encoding.t =
+  let open Data_encoding in
+  conv
+    (fun {fees_per_byte} -> fees_per_byte)
+    (fun fees_per_byte ->
+      assert (Tez_repr.(zero <= fees_per_byte)) ;
+      {fees_per_byte})
+    (obj1 (req "fees_per_byte" Tez_repr.encoding))
 
-let pp fmt () = Format.fprintf fmt "Tx_rollup: ()"
+let pp fmt {fees_per_byte} =
+  Format.fprintf fmt "Tx_rollup: fees_per_byte = %a" Tez_repr.pp fees_per_byte
+
+(* TODO: https://gitlab.com/tezos/tezos/-/issues/2338
+   To get a smoother variation of fees, that is more resistant to
+   spurious pikes of data, we will use EMA.
+
+   The type [t] probably needs to be updated accordingly. *)
+let update_fees_per_byte : t -> final_size:int -> hard_limit:int -> t =
+ fun ({fees_per_byte} as state) ~final_size ~hard_limit ->
+  let computation =
+    let open Compare.Int in
+    let percentage = final_size * 100 / hard_limit in
+    if 90 < percentage then
+      (* If the fees were null before, we bootstrap the increase with
+         a small, fix value of 100 mutez. *)
+      if Tez_repr.(fees_per_byte = zero) then ok @@ Tez_repr.of_mutez_exn 100L
+      else
+        Tez_repr.(
+          fees_per_byte *? 105L >>? fun x ->
+          x /? 100L >>? fun x -> x +? one_mutez)
+    else if 80 < percentage && percentage <= 90 then ok fees_per_byte
+    else
+      Tez_repr.(
+        fees_per_byte *? 95L >>? fun x ->
+        x /? 100L >>? fun x -> x +? one_mutez)
+  in
+  (* In the (very unlikely) event of an overflow, we keep the fees
+     constant. *)
+  match computation with
+  | Ok fees_per_byte -> {fees_per_byte}
+  | Error _ -> state
+
+let fees {fees_per_byte} size = Tez_repr.(fees_per_byte *? Int64.of_int size)
+
+module Internal_for_tests = struct
+  let initial_state_with_fees_per_byte : Tez_repr.t -> t =
+   fun fees_per_byte -> {fees_per_byte}
+end
