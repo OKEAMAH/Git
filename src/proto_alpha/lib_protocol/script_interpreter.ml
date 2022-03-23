f2b2d3f706fc9647b03251632e87521ba58d2afc
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2022-03-23 14:39:24 +0100

    Proto/Michelson: inline ilsl_nat

--- src/proto_alpha/lib_protocol/script_interpreter.ml
+++ src/proto_alpha/lib_protocol/script_interpreter.ml
@@ -405,13 +405,6 @@ and imap_iter : type a b c d e f g h. (a, b, c, d, e, f, g, h) imap_iter_type =
   (next [@ocaml.tailcall]) g gas ks accu stack
  [@@inline]
 
-and ilsl_nat : type a b c d e f. (a, b, c, d, e, f) ilsl_nat_type =
- fun g gas (kinfo, k) ks accu stack ->
-  let x = accu and (y, stack) = stack in
-  match Script_int.shift_left_n x y with
-  | None -> fail (Overflow kinfo.iloc)
-  | Some x -> (step [@ocaml.tailcall]) g gas k ks x stack
-
 and ilsr_nat : type a b c d e f. (a, b, c, d, e, f) ilsr_nat_type =
  fun g gas (kinfo, k) ks accu stack ->
   let x = accu and (y, stack) = stack in
@@ -854,7 +847,11 @@ and step : type a s b t r f. (a, s, b, t, r, f) step_type =
           let x = accu and (y, stack) = stack in
           let res = Script_int.ediv_n x y in
           (step [@ocaml.tailcall]) g gas k ks res stack
-      | ILsl_nat (kinfo, k) -> ilsl_nat g gas (kinfo, k) ks accu stack
+      | ILsl_nat (kinfo, k) -> (
+          let x = accu and (y, stack) = stack in
+          match Script_int.shift_left_n x y with
+          | None -> fail (Overflow kinfo.iloc)
+          | Some x -> (step [@ocaml.tailcall]) g gas k ks x stack)
       | ILsr_nat (kinfo, k) -> ilsr_nat g gas (kinfo, k) ks accu stack
       | IOr_nat (_, k) ->
           let x = accu and (y, stack) = stack in
