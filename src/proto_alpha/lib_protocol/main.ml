1ba0fc6e798061519d955f2eed2cf6d21e70b4c8
Fedor Sheremetyev <sheremetyev@gmail.com>
2022-04-10 18:43:16 +0000

    Jakarta: vanity nonce

--- src/proto_alpha/lib_protocol/main.ml
+++ src/proto_alpha/lib_protocol/main.ml
@@ -813,4 +813,4 @@ let precheck_manager {ctxt; _} op =
              * 'kind Alpha_context.Kind.manager
                Apply_results.prechecked_contents_list) -> ()
 
-(* Vanity nonce: 0140394843711738 *)
+(* Vanity nonce: 4199792028500268 *)
