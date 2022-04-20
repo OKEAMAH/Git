e8de5388c1a7de226d087db345b5ff1adc3e3598
François Thiré <francois.thire@nomadic-labs.com>
2022-04-20 09:29:32 +0200

    Protocol/Das: Add "das_apply.ml" file

---
+++ src/proto_alpha/lib_protocol/das_apply.mli
@@ -0,0 +1,40 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Alpha_context
+
+val validate_data_availibility : t -> Das.Endorsement.t -> unit tzresult
+
+val apply_data_availibility :
+  t ->
+  Das.Endorsement.t ->
+  endorser:Signature.Public_key_hash.t ->
+  unit tzresult Lwt.t
+
+val validate_publish_slot_header : t -> Das.Slot.t -> unit tzresult
+
+val apply_publish_slot_header : t -> Das.Slot.t -> Tez.t -> unit tzresult
+
+val das_finalisation : t -> t tzresult Lwt.t
