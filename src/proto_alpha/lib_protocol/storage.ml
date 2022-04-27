g302ge/tezos

Proto,Tx_rollup: Pre-rejections repr and storage
Co-author-by: Marigold <contact@marigold.dev>
Co-author-by: Nomadic Labs <contact@nomadic-labs.com>
Co-author-by: Oxhead Alpha <info@oxheadalpha.com>

src/proto_alpha/lib_protocol/storage.ml
@@ -1515,6 +1515,68 @@ module Tx_rollup = struct
 
         let encoding = Data_encoding.int31
       end)
+
+  module Prerejection_context =
+    Make_indexed_subcontext
+      (Make_subcontext (Registered) (Raw_context)
+         (struct
+           let name = ["tx_rollup_prerejection_index"]
+         end))
+         (Make_index (Tx_rollup_rejection_repr.Rejection_hash.Index))
+
+  module Prerejection =
+    Prerejection_context.Make_carbonated_map
+      (struct
+        let name = ["prerejection"]
+      end)
+      (struct
+        type t = Tx_rollup_repr.t * int32
+
+        let encoding =
+          Data_encoding.(
+            obj2 (req "tx_rollup" Tx_rollup_repr.encoding) (req "index" int32))
+      end)
+
+  module Prerejection_counter =
+    Indexed_context.Make_carbonated_map
+      (struct
+        let name = ["prerejection_counter"]
+      end)
+      (Encoding.Int32)
+
+  module Prerejections_by_index_context =
+    Make_subcontext (Registered) (Level_tx_rollup_context.Raw_context)
+      (struct
+        let name = ["prerejections_by_index"]
+      end)
+
+  module Prerejections_by_index =
+    Make_indexed_carbonated_data_storage
+      (Prerejections_by_index_context)
+      (Make_index (Int32_index))
+      (struct
+        type t = Tx_rollup_rejection_repr.Rejection_hash.t
+
+        let encoding = Tx_rollup_rejection_repr.Rejection_hash.encoding
+      end)
+
+  module Accepted_prerejections_context =
+    Make_subcontext (Registered) (Level_tx_rollup_context.Raw_context)
+      (struct
+        let name = ["accepted_prerejections"]
+      end)
+
+  module Accepted_prerejections = struct
+    include
+      Make_indexed_carbonated_data_storage
+        (Accepted_prerejections_context)
+        (Make_index (Tx_rollup_commitment_repr.Index))
+        (struct
+          type t = Tx_rollup_rejection_repr.prerejection
+
+          let encoding = Tx_rollup_rejection_repr.prerejection_encoding
+        end)
+  end
 end
 
 module Sc_rollup = struct

src/proto_alpha/lib_protocol/storage.mli
@@ -686,6 +686,73 @@ module Tx_rollup : sig
       (* The value here is the number of unfinalized commitments *)
        and type value = int
        and type t := Raw_context.t
+
+  (** Track prerejections.  Before a rejection can be submitted, a
+      pre-rejection must be submitted by the same contract which will submit
+      the rejection.  A pre-rejection contains a hash of the rejection with
+      the submitting contract.  Regardless of the order in which rejections
+      are submitted, the first pre-rejection submitted has priority.
+      This dramatically reduces the incentives for front-running.   Here, we
+      just store the pre-rejections themselves and their priorities, so that
+      we can quickly check their existence when a rejection is filed.*)
+  module Prerejection :
+    Non_iterable_indexed_carbonated_data_storage
+      with type key = Tx_rollup_rejection_repr.Rejection_hash.t
+       and type value = Tx_rollup_repr.t * int32
+       and type t := Raw_context.t
+
+  (** Prerejections are indexed by a counter which monotonically increases.
+      Each tx_rollup has its own counter. *)
+  module Prerejection_counter :
+    Non_iterable_indexed_carbonated_data_storage
+      with type key = Tx_rollup_repr.t
+       and type value = int32
+       and type t := Raw_context.t
+
+  (** In order to garbage-collect prerejections, we store them
+      according to the rollup level at which they were submitted.
+      This seems strange, but it lets us garbage-collect them during
+      rollup level finalization, which would otherwise be impossible.
+      Since we don't know what rollup level they are for, we
+      cannot use that. *)
+  module Prerejections_by_index : sig
+    include
+      Non_iterable_indexed_carbonated_data_storage
+        with type key = int32
+         and type value = Tx_rollup_rejection_repr.Rejection_hash.t
+         and type t :=
+              (Raw_context.t * Tx_rollup_level_repr.t) * Tx_rollup_repr.t
+
+    val list_values :
+      ?offset:int ->
+      ?length:int ->
+      (Raw_context.t * Tx_rollup_level_repr.t) * Tx_rollup_repr.t ->
+      (Raw_context.t * Tx_rollup_rejection_repr.Rejection_hash.t list) tzresult
+      Lwt.t
+  end
+
+  (** When the first rejection for a commitment is filed, we put
+      the corresponding prerejection into this table.  The level,
+      here, is the level that the commitment is for.  Later, if
+      we get any other rejections for the same commitment, and
+      those rejections have earlier prerejections, we replace
+      the accepted prerejection here. At finalization time,
+      for [level], we can credit the contract listed here. *)
+  module Accepted_prerejections : sig
+    include
+      Non_iterable_indexed_carbonated_data_storage
+        with type key = Tx_rollup_commitment_repr.Commitment_hash.t
+         and type value = Tx_rollup_rejection_repr.prerejection
+         and type t :=
+              (Raw_context.t * Tx_rollup_level_repr.t) * Tx_rollup_repr.t
+
+    val list_values :
+      ?offset:int ->
+      ?length:int ->
+      (Raw_context.t * Tx_rollup_level_repr.t) * Tx_rollup_repr.t ->
+      (Raw_context.t * Tx_rollup_rejection_repr.prerejection list) tzresult
+      Lwt.t
+  end
 end
 
 module Sc_rollup : sig

src/proto_alpha/lib_protocol/tx_rollup_rejection_repr.ml
@@ -1,0 +1,131 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+module Rejection_hash = struct
+  let rejection_hash = "\001\111\092\025" (* rej1(37) *)
+
+  module H =
+    Blake2B.Make
+      (Base58)
+      (struct
+        let name = "Rejection_hash"
+
+        let title = "A rejection ID"
+
+        let b58check_prefix = rejection_hash
+
+        let size = Some 20
+      end)
+
+  include H
+
+  let () = Base58.check_encoded_prefix b58check_encoding "rej1" 37
+
+  include Path_encoding.Make_hex (H)
+
+  let rpc_arg =
+    let construct = Data_encoding.Binary.to_string_exn encoding in
+    let destruct str =
+      Option.value_e ~error:"Failed to decode rejection"
+      @@ Data_encoding.Binary.of_string_opt encoding str
+    in
+    RPC_arg.make
+      ~descr:"A tx_rollup rejection."
+      ~name:"tx_rollup_rejection"
+      ~construct
+      ~destruct
+      ()
+
+  module Index = struct
+    type nonrec t = t
+
+    let path_length = 1
+
+    let to_path c l =
+      let raw_key = Data_encoding.Binary.to_bytes_exn encoding c in
+      let (`Hex key) = Hex.of_bytes raw_key in
+      key :: l
+
+    let of_path = function
+      | [key] ->
+          Option.bind
+            (Hex.to_bytes (`Hex key))
+            (Data_encoding.Binary.of_bytes_opt encoding)
+      | _ -> None
+
+    let rpc_arg = rpc_arg
+
+    let encoding = encoding
+
+    let compare = compare
+  end
+end
+
+let generate_prerejection :
+    source:Signature.Public_key_hash.t ->
+    tx_rollup:Tx_rollup_repr.t ->
+    level:Tx_rollup_level_repr.t ->
+    message_position:int ->
+    proof:Tx_rollup_l2_proof.t ->
+    Rejection_hash.t =
+ fun ~source ~tx_rollup ~level ~message_position ~proof ->
+  let to_bytes = Data_encoding.Binary.to_bytes_exn in
+  let rollup_bytes = to_bytes Tx_rollup_repr.encoding tx_rollup in
+  let level_bytes = to_bytes Tx_rollup_level_repr.encoding level in
+  let proof_bytes =
+    Data_encoding.Binary.to_bytes_exn Tx_rollup_l2_proof.encoding proof
+  in
+  let message_position_bytes = to_bytes Data_encoding.int31 message_position in
+  let contract_bytes = to_bytes Signature.Public_key_hash.encoding source in
+  Rejection_hash.hash_bytes
+    [
+      rollup_bytes;
+      level_bytes;
+      proof_bytes;
+      message_position_bytes;
+      contract_bytes;
+    ]
+
+type prerejection = {
+  hash : Tx_rollup_commitment_repr.Commitment_hash.t;
+  proof : Tx_rollup_l2_proof.t;
+  contract : Signature.Public_key_hash.t;
+  priority : int32;
+}
+
+let prerejection_encoding =
+  Data_encoding.(
+    conv
+      (fun {hash; contract; priority; proof} ->
+        (hash, contract, priority, proof))
+      (fun (hash, contract, priority, proof) ->
+        {hash; contract; priority; proof})
+      (obj4
+         (req "hash" Tx_rollup_commitment_repr.Commitment_hash.encoding)
+         (req "contract" Signature.Public_key_hash.encoding)
+         (req "priority" int32)
+         (req "proof" Tx_rollup_l2_proof.encoding)))

src/proto_alpha/lib_protocol/tx_rollup_rejection_repr.mli
@@ -1,8 +1,9 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
 (* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
-(* Copyright (c) 2022 Oxhead Alpha <info@oxheadalpha.com>                    *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,9 +25,27 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** TODO/TORU: replace with actual proof type and add a docstring *)
-type t = bool
+module Rejection_hash : sig
+  val rejection_hash : string
 
-val encoding : t Data_encoding.t
+  include S.HASH
 
-val ( = ) : t -> t -> bool
+  module Index : Storage_description.INDEX with type t = t
+end
+
+val generate_prerejection :
+  source:Signature.Public_key_hash.t ->
+  tx_rollup:Tx_rollup_repr.t ->
+  level:Tx_rollup_level_repr.t ->
+  message_position:int ->
+  proof:Tx_rollup_l2_proof.t ->
+  Rejection_hash.t
+
+type prerejection = {
+  hash : Tx_rollup_commitment_repr.Commitment_hash.t;
+  proof : Tx_rollup_l2_proof.t;
+  contract : Signature.Public_key_hash.t;
+  priority : int32;
+}
+
+val prerejection_encoding : prerejection Data_encoding.t

src/proto_alpha/lib_protocol/tx_rollup_rejection_storage.mli
@@ -1,8 +1,9 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
 (* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
-(* Copyright (c) 2022 Oxhead Alpha <info@oxheadalpha.com>                    *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,9 +25,29 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** TODO/TORU: replace with actual proof type and add a docstring *)
-type t = bool
+(** [prereject ctxt tx_rollup hash] stores a prerejection *)
+val prereject :
+  Raw_context.t ->
+  Tx_rollup_repr.t ->
+  Tx_rollup_rejection_repr.Rejection_hash.t ->
+  Raw_context.t tzresult Lwt.t
 
-val encoding : t Data_encoding.t
+val check_prerejection :
+  Raw_context.t ->
+  source:Signature.Public_key_hash.t ->
+  tx_rollup:Tx_rollup_repr.t ->
+  level:Tx_rollup_level_repr.t ->
+  message_position:int ->
+  proof:Tx_rollup_l2_proof.t ->
+  (Raw_context.t * int32) tzresult Lwt.t
 
-val ( = ) : t -> t -> bool
+val update_accepted_prerejection :
+  Raw_context.t ->
+  source:Signature.Public_key_hash.t ->
+  tx_rollup:Tx_rollup_repr.t ->
+  level:Tx_rollup_level_repr.t ->
+  commitment:Tx_rollup_commitment_repr.Commitment_hash.t ->
+  commitment_exists:bool ->
+  proof:Tx_rollup_l2_proof.t ->
+  priority:int32 ->
+  Raw_context.t tzresult Lwt.t

OKEAMAH/Git

src/proto_alpha/lib_protocol/storage.ml
@@ -1443,6 +1443,24 @@ module Tx_rollup = struct
       end)
       (Bitset)
 
+  module Int32_index = struct
+    type t = int32
+
+    let compare = Compare.Int32.compare
+
+    let encoding = Data_encoding.int32
+
+    let rpc_arg = RPC_arg.int32
+
+    let path_length = 1
+
+    let to_path i l = Int32.to_string i :: l
+
+    let of_path = function
+      | [] | _ :: _ :: _ -> None
+      | [i] -> Int32.of_string_opt i
+  end
+
   module Commitment =
     Level_context.Make_carbonated_map
       (struct
@@ -1468,6 +1486,68 @@ module Tx_rollup = struct
 
         let encoding = Data_encoding.int31
       end)
+
+  module Prerejection_context =
+    Make_indexed_subcontext
+      (Make_subcontext (Registered) (Raw_context)
+         (struct
+           let name = ["tx_rollup_prerejection_index"]
+         end))
+         (Make_index (Tx_rollup_rejection_repr.Rejection_hash.Index))
+
+  module Prerejection =
+    Prerejection_context.Make_carbonated_map
+      (struct
+        let name = ["prerejection"]
+      end)
+      (struct
+        type t = Tx_rollup_repr.t * int32
+
+        let encoding =
+          Data_encoding.(
+            obj2 (req "tx_rollup" Tx_rollup_repr.encoding) (req "index" int32))
+      end)
+
+  module Prerejection_counter =
+    Indexed_context.Make_carbonated_map
+      (struct
+        let name = ["prerejection_counter"]
+      end)
+      (Encoding.Int32)
+
+  module Prerejections_by_index_context =
+    Make_subcontext (Registered) (Level_context.Raw_context)
+      (struct
+        let name = ["prerejections_by_index"]
+      end)
+
+  module Prerejections_by_index =
+    Make_indexed_carbonated_data_storage
+      (Prerejections_by_index_context)
+      (Make_index (Int32_index))
+      (struct
+        type t = Tx_rollup_rejection_repr.Rejection_hash.t
+
+        let encoding = Tx_rollup_rejection_repr.Rejection_hash.encoding
+      end)
+
+  module Accepted_prerejections_context =
+    Make_subcontext (Registered) (Level_context.Raw_context)
+      (struct
+        let name = ["accepted_prerejections"]
+      end)
+
+  module Accepted_prerejections = struct
+    include
+      Make_indexed_carbonated_data_storage
+        (Accepted_prerejections_context)
+        (Make_index (Tx_rollup_commitment_repr.Index))
+        (struct
+          type t = Tx_rollup_rejection_repr.prerejection
+
+          let encoding = Tx_rollup_rejection_repr.prerejection_encoding
+        end)
+  end
 end
 
 module Sc_rollup = struct

src/proto_alpha/lib_protocol/storage.mli
@@ -654,6 +654,73 @@ module Tx_rollup : sig
       with type key = Signature.public_key_hash
        and type value = int
        and type t := Raw_context.t * Tx_rollup_repr.t
+
+  (** Track prerejections.  Before a rejection can be submitted, a
+      pre-rejection must be submitted by the same contract which will submit
+      the rejection.  A pre-rejection contains a hash of the rejection with
+      the submitting contract.  Regardless of the order in which rejections
+      are submitted, the first pre-rejection submitted has priority.
+      This dramatically reduces the incentives for front-running.   Here, we
+      just store the pre-rejections themselves and their priorities, so that
+      we can quickly check their existence when a rejection is filed.*)
+  module Prerejection :
+    Non_iterable_indexed_carbonated_data_storage
+      with type key = Tx_rollup_rejection_repr.Rejection_hash.t
+       and type value = Tx_rollup_repr.t * int32
+       and type t := Raw_context.t
+
+  (** Prerejections are indexed by a counter which monotonically increases.
+      Each tx_rollup has its own counter. *)
+  module Prerejection_counter :
+    Non_iterable_indexed_carbonated_data_storage
+      with type key = Tx_rollup_repr.t
+       and type value = int32
+       and type t := Raw_context.t
+
+  (** In order to garbage-collect prerejections, we store them
+      according to the rollup level at which they were submitted.
+      This seems strange, but it lets us garbage-collect them during
+      rollup level finalization, which would otherwise be impossible.
+      Since we don't know what rollup level they are for, we
+      cannot use that. *)
+  module Prerejections_by_index : sig
+    include
+      Non_iterable_indexed_carbonated_data_storage
+        with type key = int32
+         and type value = Tx_rollup_rejection_repr.Rejection_hash.t
+         and type t :=
+              (Raw_context.t * Tx_rollup_repr.t) * Tx_rollup_level_repr.t
+
+    val list_values :
+      ?offset:int ->
+      ?length:int ->
+      (Raw_context.t * Tx_rollup_repr.t) * Tx_rollup_level_repr.t ->
+      (Raw_context.t * Tx_rollup_rejection_repr.Rejection_hash.t list) tzresult
+      Lwt.t
+  end
+
+  (** When the first rejection for a commitment is filed, we put
+      the corresponding prerejection into this table.  The level,
+      here, is the level that the commitment is for.  Later, if
+      we get any other rejections for the same commitment, and
+      those rejections have earlier prerejections, we replace
+      the accepted prerejection here. At finalization time,
+      for [level], we can credit the contract listed here. *)
+  module Accepted_prerejections : sig
+    include
+      Non_iterable_indexed_carbonated_data_storage
+        with type key = Tx_rollup_commitment_repr.Hash.t
+         and type value = Tx_rollup_rejection_repr.prerejection
+         and type t :=
+              (Raw_context.t * Tx_rollup_repr.t) * Tx_rollup_level_repr.t
+
+    val list_values :
+      ?offset:int ->
+      ?length:int ->
+      (Raw_context.t * Tx_rollup_repr.t) * Tx_rollup_level_repr.t ->
+      (Raw_context.t * Tx_rollup_rejection_repr.prerejection list) tzresult
+      Lwt.t
+  end
 end
 
 module Sc_rollup : sig

src/proto_alpha/lib_protocol/tx_rollup_rejection_repr.ml
@@ -1,0 +1,131 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+module Rejection_hash = struct
+  let rejection_hash = "\001\111\092\025" (* rej1(37) *)
+
+  module H =
+    Blake2B.Make
+      (Base58)
+      (struct
+        let name = "Rejection_hash"
+
+        let title = "A rejection ID"
+
+        let b58check_prefix = rejection_hash
+
+        let size = Some 20
+      end)
+
+  include H
+
+  let () = Base58.check_encoded_prefix b58check_encoding "rej1" 37
+
+  include Path_encoding.Make_hex (H)
+
+  let rpc_arg =
+    let construct = Data_encoding.Binary.to_string_exn encoding in
+    let destruct str =
+      Option.value_e ~error:"Failed to decode rejection"
+      @@ Data_encoding.Binary.of_string_opt encoding str
+    in
+    RPC_arg.make
+      ~descr:"A tx_rollup rejection."
+      ~name:"tx_rollup_rejection"
+      ~construct
+      ~destruct
+      ()
+
+  module Index = struct
+    type nonrec t = t
+
+    let path_length = 1
+
+    let to_path c l =
+      let raw_key = Data_encoding.Binary.to_bytes_exn encoding c in
+      let (`Hex key) = Hex.of_bytes raw_key in
+      key :: l
+
+    let of_path = function
+      | [key] ->
+          Option.bind
+            (Hex.to_bytes (`Hex key))
+            (Data_encoding.Binary.of_bytes_opt encoding)
+      | _ -> None
+
+    let rpc_arg = rpc_arg
+
+    let encoding = encoding
+
+    let compare = compare
+  end
+end
+
+let generate_prerejection :
+    source:Signature.Public_key_hash.t ->
+    tx_rollup:Tx_rollup_repr.t ->
+    level:Tx_rollup_level_repr.t ->
+    message_position:int ->
+    proof:Tx_rollup_l2_proof.t ->
+    Rejection_hash.t =
+ fun ~source ~tx_rollup ~level ~message_position ~proof ->
+  let to_bytes = Data_encoding.Binary.to_bytes_exn in
+  let rollup_bytes = to_bytes Tx_rollup_repr.encoding tx_rollup in
+  let level_bytes = to_bytes Tx_rollup_level_repr.encoding level in
+  let proof_bytes =
+    Data_encoding.Binary.to_bytes_exn Tx_rollup_l2_proof.encoding proof
+  in
+  let message_position_bytes = to_bytes Data_encoding.int31 message_position in
+  let contract_bytes = to_bytes Signature.Public_key_hash.encoding source in
+  Rejection_hash.hash_bytes
+    [
+      rollup_bytes;
+      level_bytes;
+      proof_bytes;
+      message_position_bytes;
+      contract_bytes;
+    ]
+
+type prerejection = {
+  hash : Tx_rollup_commitment_repr.Hash.t;
+  proof : Tx_rollup_l2_proof.t;
+  contract : Signature.Public_key_hash.t;
+  priority : int32;
+}
+
+let prerejection_encoding =
+  Data_encoding.(
+    conv
+      (fun {hash; contract; priority; proof} ->
+        (hash, contract, priority, proof))
+      (fun (hash, contract, priority, proof) ->
+        {hash; contract; priority; proof})
+      (obj4
+         (req "hash" Tx_rollup_commitment_repr.Hash.encoding)
+         (req "contract" Signature.Public_key_hash.encoding)
+         (req "priority" int32)
+         (req "proof" Tx_rollup_l2_proof.encoding)))

src/proto_alpha/lib_protocol/tx_rollup_rejection_repr.mli
@@ -1,8 +1,9 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
 (* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
-(* Copyright (c) 2022 Oxhead Alpha <info@oxheadalpha.com>                    *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,9 +25,27 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(* The type of a Merkle proof for a L2 message *)
-type t = Context.Proof.stream Context.Proof.t
+module Rejection_hash : sig
+  val rejection_hash : string
 
-val encoding : t Data_encoding.t
+  include S.HASH
 
-val ( = ) : t -> t -> bool
+  module Index : Storage_description.INDEX with type t = t
+end
+
+val generate_prerejection :
+  source:Signature.Public_key_hash.t ->
+  tx_rollup:Tx_rollup_repr.t ->
+  level:Tx_rollup_level_repr.t ->
+  message_position:int ->
+  proof:Tx_rollup_l2_proof.t ->
+  Rejection_hash.t
+
+type prerejection = {
+  hash : Tx_rollup_commitment_repr.Hash.t;
+  proof : Tx_rollup_l2_proof.t;
+  contract : Signature.Public_key_hash.t;
+  priority : int32;
+}
+
+val prerejection_encoding : prerejection Data_encoding.t

src/proto_alpha/lib_protocol/tx_rollup_rejection_storage.mli
@@ -1,8 +1,9 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
 (* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
-(* Copyright (c) 2022 Oxhead Alpha <info@oxheadalpha.com>                    *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,9 +25,29 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(* The type of a Merkle proof for a L2 message *)
-type t = Context.Proof.stream Context.Proof.t
+(** [prereject ctxt tx_rollup hash] stores a prerejection *)
+val prereject :
+  Raw_context.t ->
+  Tx_rollup_repr.t ->
+  Tx_rollup_rejection_repr.Rejection_hash.t ->
+  Raw_context.t tzresult Lwt.t
 
-val encoding : t Data_encoding.t
+val check_prerejection :
+  Raw_context.t ->
+  source:Signature.Public_key_hash.t ->
+  tx_rollup:Tx_rollup_repr.t ->
+  level:Tx_rollup_level_repr.t ->
+  message_position:int ->
+  proof:Tx_rollup_l2_proof.t ->
+  (Raw_context.t * int32) tzresult Lwt.t
 
-val ( = ) : t -> t -> bool
+val update_accepted_prerejection :
+  Raw_context.t ->
+  source:Signature.Public_key_hash.t ->
+  tx_rollup:Tx_rollup_repr.t ->
+  level:Tx_rollup_level_repr.t ->
+  commitment:Tx_rollup_commitment_repr.Hash.t ->
+  commitment_exists:bool ->
+  proof:Tx_rollup_l2_proof.t ->
+  priority:int32 ->
+  Raw_context.t tzresult Lwt.t