g302ge/tezos

Proto,Tx_rollup: Prerejection operation
Co-author-by: Marigold <contact@marigold.dev>
Co-author-by: Nomadic Labs <contact@nomadic-labs.com>
Co-author-by: Oxhead Alpha <info@oxheadalpha.com>

src/proto_alpha/lib_protocol/operation_repr.mli
@@ -115,6 +115,8 @@ module Kind : sig
 
   type tx_rollup_withdraw = Tx_rollup_withdraw_kind
 
+  type tx_rollup_prerejection = Tx_rollup_prerejection_kind
+
   type sc_rollup_originate = Sc_rollup_originate_kind
 
   type sc_rollup_add_messages = Sc_rollup_add_messages_kind
@@ -138,6 +140,7 @@ module Kind : sig
         : tx_rollup_remove_commitment manager
     | Tx_rollup_rejection_manager_kind : tx_rollup_rejection manager
     | Tx_rollup_withdraw_manager_kind : tx_rollup_withdraw manager
+    | Tx_rollup_prerejection_manager_kind : tx_rollup_prerejection manager
     | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
     | Sc_rollup_add_messages_manager_kind : sc_rollup_add_messages manager
     | Sc_rollup_cement_manager_kind : sc_rollup_cement manager
@@ -368,6 +371,7 @@ and _ manager_operation =
       message_position : int;
       previous_message_result : Tx_rollup_commitment_repr.message_result;
       proof : Tx_rollup_l2_proof.t;
+      commitment : Tx_rollup_commitment_repr.Commitment_hash.t;
     }
       -> Kind.tx_rollup_rejection manager_operation
       (** [Tx_rollup_withdraw] allows an implicit account (the "claimer") to
@@ -404,6 +408,11 @@ and _ manager_operation =
           (** The entrypoint of the smart contract address that should receive the tickets. *)
     }
       -> Kind.tx_rollup_withdraw manager_operation
+  | Tx_rollup_prerejection : {
+      tx_rollup : Tx_rollup_repr.t;
+      hash : Tx_rollup_rejection_repr.Rejection_hash.t;
+    }
+      -> Kind.tx_rollup_prerejection manager_operation
   (* [Sc_rollup_originate] allows an implicit account to originate a new
      smart contract rollup (initialized with a given boot
      sector). *)
@@ -553,6 +562,9 @@ module Encoding : sig
 
   val tx_rollup_withdraw_case : Kind.tx_rollup_withdraw Kind.manager case
 
+  val tx_rollup_prerejection_case :
+    Kind.tx_rollup_prerejection Kind.manager case
+
   val sc_rollup_originate_case : Kind.sc_rollup_originate Kind.manager case
 
   val sc_rollup_add_messages_case :
@@ -606,6 +618,8 @@ module Encoding : sig
     val tx_rollup_rejection_case : Kind.tx_rollup_rejection case
 
     val tx_rollup_withdraw_case : Kind.tx_rollup_withdraw case
+
+    val tx_rollup_prerejection_case : Kind.tx_rollup_prerejection case
 
     val sc_rollup_originate_case : Kind.sc_rollup_originate case
 

src/proto_alpha/lib_protocol/tx_rollup_commitment_storage.ml
@@ -387,3 +387,18 @@ let reject_commitment ctxt rollup state level =
       Tx_rollup_state_repr.record_commitment_rejection state level pred_hash
       >>?= fun state -> return (ctxt, state)
   | _ -> fail Invalid_rejection_level_argument
+
+let commitment_exists :
+    Raw_context.t ->
+    Tx_rollup_repr.t ->
+    Tx_rollup_level_repr.t ->
+    Commitment_hash.t ->
+    (Raw_context.t * bool) tzresult Lwt.t =
+ fun ctxt tx_rollup level commitment_to_check ->
+  find ctxt tx_rollup level >>=? fun (ctxt, commitment) ->
+  match commitment with
+  | None -> return (ctxt, false)
+  | Some commitment ->
+      return
+        ( ctxt,
+          Commitment_hash.(commitment.commitment_hash = commitment_to_check) )

src/proto_alpha/lib_protocol/tx_rollup_commitment_storage.mli
@@ -179,3 +179,13 @@ val get_before_and_after_results :
   * Tx_rollup_commitment_repr.Message_result_hash.t)
   tzresult
   Lwt.t
+
+(** [commitment_exists ctxt tx_rollup level commitment_hash]
+    checks that the commitment at level [level] is equal to
+    [commitment_hash]. *)
+val commitment_exists :
+  Raw_context.t ->
+  Tx_rollup_repr.t ->
+  Tx_rollup_level_repr.t ->
+  Tx_rollup_commitment_repr.Commitment_hash.t ->
+  (Raw_context.t * bool) tzresult Lwt.t

OKEAMAH/Git

src/proto_alpha/lib_protocol/operation_repr.mli
@@ -118,6 +118,8 @@ module Kind : sig
 
   type transfer_ticket = Transfer_ticket_kind
 
+  type tx_rollup_prerejection = Tx_rollup_prerejection_kind
+
   type sc_rollup_originate = Sc_rollup_originate_kind
 
   type sc_rollup_add_messages = Sc_rollup_add_messages_kind
@@ -145,6 +147,7 @@ module Kind : sig
     | Tx_rollup_dispatch_tickets_manager_kind
         : tx_rollup_dispatch_tickets manager
     | Transfer_ticket_manager_kind : transfer_ticket manager
+    | Tx_rollup_prerejection_manager_kind : tx_rollup_prerejection manager
     | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
     | Sc_rollup_add_messages_manager_kind : sc_rollup_add_messages manager
     | Sc_rollup_cement_manager_kind : sc_rollup_cement manager
@@ -379,6 +382,7 @@ and _ manager_operation =
       previous_message_result : Tx_rollup_message_result_repr.t;
       previous_message_result_path : Tx_rollup_commitment_repr.Merkle.path;
       proof : Tx_rollup_l2_proof.t;
+      commitment : Tx_rollup_commitment_repr.Hash.t;
     }
       -> Kind.tx_rollup_rejection manager_operation
   | Tx_rollup_dispatch_tickets : {
@@ -417,6 +421,11 @@ and _ manager_operation =
           (** The entrypoint of the smart contract address that should receive the tickets. *)
     }
       -> Kind.transfer_ticket manager_operation
+  | Tx_rollup_prerejection : {
+      tx_rollup : Tx_rollup_repr.t;
+      hash : Tx_rollup_rejection_repr.Rejection_hash.t;
+    }
+      -> Kind.tx_rollup_prerejection manager_operation
   (* [Sc_rollup_originate] allows an implicit account to originate a new
      smart contract rollup (initialized with a given boot
      sector). *)
@@ -574,6 +583,9 @@ module Encoding : sig
 
   val transfer_ticket_case : Kind.transfer_ticket Kind.manager case
 
+  val tx_rollup_prerejection_case :
+    Kind.tx_rollup_prerejection Kind.manager case
+
   val sc_rollup_originate_case : Kind.sc_rollup_originate Kind.manager case
 
   val sc_rollup_add_messages_case :
@@ -631,6 +643,8 @@ module Encoding : sig
     val tx_rollup_dispatch_tickets_case : Kind.tx_rollup_dispatch_tickets case
 
     val transfer_ticket_case : Kind.transfer_ticket case
+
+    val tx_rollup_prerejection_case : Kind.tx_rollup_prerejection case
 
     val sc_rollup_originate_case : Kind.sc_rollup_originate case
 

src/proto_alpha/lib_protocol/tx_rollup_commitment_storage.ml
@@ -410,3 +410,17 @@ let reject_commitment ctxt rollup state level =
   >>=? fun (ctxt, pred_hash) ->
   Tx_rollup_state_repr.record_commitment_rejection state level pred_hash
   >>?= fun state -> return (ctxt, state)
+
+let commitment_exists :
+    Raw_context.t ->
+    Tx_rollup_repr.t ->
+    Tx_rollup_state_repr.t ->
+    Tx_rollup_level_repr.t ->
+    Hash.t ->
+    (Raw_context.t * bool) tzresult Lwt.t =
+ fun ctxt tx_rollup state level commitment_to_check ->
+  find ctxt tx_rollup state level >>=? fun (ctxt, commitment) ->
+  match commitment with
+  | None -> return (ctxt, false)
+  | Some commitment ->
+      return (ctxt, Hash.(commitment.commitment_hash = commitment_to_check))

src/proto_alpha/lib_protocol/tx_rollup_commitment_storage.mli
@@ -195,3 +195,14 @@ val check_agreed_and_disputed_results :
   disputed_position:int ->
   disputed_result_path:Tx_rollup_commitment_repr.Merkle.path ->
   Raw_context.t tzresult Lwt.t
+
+(** [commitment_exists ctxt tx_rollup level commitment_hash]
+    checks that the commitment at level [level] is equal to
+    [commitment_hash]. *)
+val commitment_exists :
+  Raw_context.t ->
+  Tx_rollup_repr.t ->
+  Tx_rollup_state_repr.t ->
+  Tx_rollup_level_repr.t ->
+  Tx_rollup_commitment_repr.Hash.t ->
+  (Raw_context.t * bool) tzresult Lwt.t