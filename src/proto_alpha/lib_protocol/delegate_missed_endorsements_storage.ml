7df666dff460d2e945ac21485878e1b4d3e512df
G.B. Fefe <gb.fefe@protonmail.com>
2022-05-07 13:08:21 +0200

    Proto: no error for `expected_slots_for_given_active_stake`

--- src/proto_alpha/lib_protocol/delegate_missed_endorsements_storage.ml
+++ src/proto_alpha/lib_protocol/delegate_missed_endorsements_storage.ml
@@ -36,13 +36,12 @@ let expected_slots_for_given_active_stake ctxt ~total_active_stake ~active_stake
   let number_of_endorsements_per_cycle =
     blocks_per_cycle * consensus_committee_size
   in
-  Result.return
-    (Z.to_int
-       (Z.div
-          (Z.mul
-             (Z.of_int64 (Tez_repr.to_mutez active_stake))
-             (Z.of_int number_of_endorsements_per_cycle))
-          (Z.of_int64 (Tez_repr.to_mutez total_active_stake))))
+  Z.to_int
+    (Z.div
+       (Z.mul
+          (Z.of_int64 (Tez_repr.to_mutez active_stake))
+          (Z.of_int number_of_endorsements_per_cycle))
+       (Z.of_int64 (Tez_repr.to_mutez total_active_stake)))
 
 type level_participation = Participated | Didn't_participate
 
@@ -79,11 +78,12 @@ let record_endorsing_participation ctxt ~delegate ~participation
           | Some active_stake ->
               Stake_storage.get_total_active_stake ctxt level.cycle
               >>=? fun total_active_stake ->
-              expected_slots_for_given_active_stake
-                ctxt
-                ~total_active_stake
-                ~active_stake
-              >>?= fun expected_slots ->
+              let expected_slots =
+                expected_slots_for_given_active_stake
+                  ctxt
+                  ~total_active_stake
+                  ~active_stake
+              in
               let Ratio_repr.{numerator; denominator} =
                 Constants_storage.minimal_participation_ratio ctxt
               in
@@ -157,11 +157,12 @@ let participation_info ctxt delegate =
   | Some active_stake ->
       Stake_storage.get_total_active_stake ctxt level.cycle
       >>=? fun total_active_stake ->
-      expected_slots_for_given_active_stake
-        ctxt
-        ~total_active_stake
-        ~active_stake
-      >>?= fun expected_cycle_activity ->
+      let expected_cycle_activity =
+        expected_slots_for_given_active_stake
+          ctxt
+          ~total_active_stake
+          ~active_stake
+      in
       let Ratio_repr.{numerator; denominator} =
         Constants_storage.minimal_participation_ratio ctxt
       in
@@ -234,11 +235,12 @@ let distribute_endorsing_rewards ctxt last_cycle unrevealed_nonces =
       let has_revealed_nonces =
         delegate_has_revealed_nonces delegate unrevealed_nonces_set
       in
-      expected_slots_for_given_active_stake
-        ctxt
-        ~total_active_stake
-        ~active_stake
-      >>?= fun expected_slots ->
+      let expected_slots =
+        expected_slots_for_given_active_stake
+          ctxt
+          ~total_active_stake
+          ~active_stake
+      in
       let rewards = Tez_repr.mul_exn endorsing_reward_per_slot expected_slots in
       (if sufficient_participation && has_revealed_nonces then
        (* Sufficient participation: we pay the rewards *)
