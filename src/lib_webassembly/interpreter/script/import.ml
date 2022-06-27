5fcd86ba0a632e948f2630bcb8de01ba3cb0d7bf
Ole Krüger <ole.kruger@trili.tech>
2022-06-27 20:53:56 +0100

    fixup: Properly catch out missing imports/exports

--- src/lib_webassembly/interpreter/script/import.ml
+++ src/lib_webassembly/interpreter/script/import.ml
@@ -14,9 +14,13 @@ let register name lookup = registry := Registry.add name lookup !registry
 let lookup (m : module_) (im : import) : Instance.extern Lwt.t =
   let {module_name; item_name; idesc} = im.it in
   let t = import_type m im in
-  try Registry.find module_name !registry item_name t with Not_found ->
-    Unknown.error im.at
-      ("unknown import \"" ^ string_of_name module_name ^
-        "\".\"" ^ string_of_name item_name ^ "\"")
+  Lwt.catch
+    (fun () -> Registry.find module_name !registry item_name t)
+    (function
+    | Not_found ->
+      Unknown.error im.at
+        ("unknown import \"" ^ string_of_name module_name ^
+          "\".\"" ^ string_of_name item_name ^ "\"")
+    | exn -> raise exn)
 
 let link m = TzStdLib.List.map_s (lookup m) m.it.imports
