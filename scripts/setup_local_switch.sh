04a314831cf7af66bf084d9e6cba77b400a0ea35
Marek Kubica <marek@tarides.com>
2022-07-01 10:55:16 +0200

    Use Tezos repository as source for packages

--- scripts/setup_local_switch.sh
+++ scripts/setup_local_switch.sh
@@ -5,13 +5,28 @@ set -eu
 script_dir="$(cd "$(dirname "$0")" && echo "$(pwd -P)/")"
 src_dir="$(dirname "$script_dir")"
 
+#shellcheck source=scripts/version.sh
+. "$script_dir"/version.sh
+
+opam repository set-url tezos --dont-select "$opam_repository" || \
+    opam repository add tezos --dont-select "$opam_repository" > /dev/null 2>&1
+
 dune_universe="git+https://github.com/dune-universe/opam-overlays.git"
+
+# Adds the dune-universe/opam-overlays repository for opam-monorepo, allowing it
+# to use dune ports when upstream packages don't build with dune
+opam repository set-url dune-universe --dont-select "$dune_universe" || \
+    opam repository add dune-universe --dont-select "$dune_universe" > /dev/null 2>&1
+
+switch_create () {
+  opam switch create ./ --empty -y --repositories=tezos,dune-universe
+}
 
 cd "$src_dir"
 if [ ! -d "_opam" ]; then
   # Creates a local switch if there's none
   echo "Create a fresh local switch"
-  opam switch create ./ --empty -y
+  switch_create
 else
   if [ "$(opam switch invariant)" != "[]" ]; then
     # If a local switch exists with an invariant, it likely pre-dates
@@ -19,10 +34,7 @@ else
     # invariant
     echo "A local switch with an invariant already exists"
     opam switch remove ./
-    opam switch create ./ --empty -y
+    switch_create
   fi
 fi
-# Adds the dune-universe/opam-overlays repository for opam-monorepo, allowing it
-# to use dune ports when upstream packages don't build with dune
-opam repository add dune-universe $dune_universe 2> /dev/null
 opam update --quiet > /dev/null
