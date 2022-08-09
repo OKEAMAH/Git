ef7d157b3336f07535fc5b2c459a93d1fae00f4c
Hans Hoglund <hans.hoglund@trili.tech>
2022-08-09 10:48:01 +0000

    Scoru: Wasm: Refactor tests
    The context argument always refers to an empty context

--- src/proto_alpha/lib_protocol/test/integration/test_sc_rollup_wasm.ml
+++ src/proto_alpha/lib_protocol/test/integration/test_sc_rollup_wasm.ml
@@ -351,15 +351,15 @@ let should_boot_incomplete_boot_sector kernel () =
   let final_chunk_size = Bytes.length final_chunk in
 
   let*! index = Context_binary.init "/tmp" in
-  let context = Context_binary.empty index in
-  let*! s = Prover.initial_state context in
+  let empty_context = Context_binary.empty index in
+  let*! s = Prover.initial_state empty_context in
   let*! s = Prover.install_boot_sector s initial_chunk in
-  let* () = check_proof_size ~loc:__LOC__ context None s in
+  let* () = check_proof_size ~loc:__LOC__ empty_context None s in
   let*! () = check_status s None in
   let* () = check_chunks_count s 0 in
   (* First tick, to interpret the boot sector. One chunk have been
      provided, and the PVM expects more chunk to come. *)
-  let* s = checked_eval ~loc:__LOC__ context s in
+  let* s = checked_eval ~loc:__LOC__ empty_context s in
   let*! () = check_status s (Some (Gathering_floppies operator.pk)) in
   let* () = check_chunks_count s chunk_size in
   (* Then, installing the additional chunks. *)
@@ -369,7 +369,7 @@ let should_boot_incomplete_boot_sector kernel () =
         (* We are installing the [i+2]th chunk ([i] starts at 0, and
            the first chunk is not part of the list). *)
         let* input = floppy_input i operator chunk in
-        let* s = checked_set_input ~loc:__LOC__ context input s in
+        let* s = checked_set_input ~loc:__LOC__ empty_context input s in
         (* We have [i+2] chunks. *)
         let* () = check_chunks_count s ((i + 2) * chunk_size) in
         return s)
@@ -379,7 +379,7 @@ let should_boot_incomplete_boot_sector kernel () =
   (* Up until the very last one, where the status of the PVM change. *)
   let len = List.length chunks in
   let* input = floppy_input len operator final_chunk in
-  let* s = checked_set_input ~loc:__LOC__ context input s in
+  let* s = checked_set_input ~loc:__LOC__ empty_context input s in
   let*! () = check_status s (Some Not_gathering_floppies) in
   let* () =
     check_chunks_count s (((len + 1) * chunk_size) + final_chunk_size)
