f213738a9e1803c5a85d57d0d7477dccff4c25b0
Alain Mebsout <alain.mebsout@functori.com>
2022-08-30 16:47:36 +0200

    Crypto: Versioned Signature module
    - V0 is the old Signature
    - V1 is a duplicate of V0 (for now)
    - V_latest is a pointer to V1

--- src/lib_crypto/signature_aliases.ml
+++ src/lib_crypto/signature_aliases.ml
@@ -1,8 +1,6 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
-(* Copyright (c) 2020 Metastate AG <hello@metastate.dev>                     *)
 (* Copyright (c) 2022 Nomadic Labs. <contact@nomadic-labs.com>               *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
@@ -25,35 +23,12 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-module V0 = Signature_v0
-module V1 = Signature_v1
-
-module type CONV = sig
-  module V_from : S.COMMON_SIGNATURE
-
-  module V_to : S.COMMON_SIGNATURE
-
-  val public_key_hash : V_from.Public_key_hash.t -> V_to.Public_key_hash.t
-
-  val public_key : V_from.Public_key.t -> V_to.Public_key.t
-
-  val secret_key : V_from.Secret_key.t -> V_to.Secret_key.t
-
-  val signature : V_from.t -> V_to.t
+(** Module to define the alias [Signature] as {!Signature.V0}. *)
+module V0 = struct
+  module Signature = Signature.V0
 end
 
-module V_latest = struct
-  include V1
-
-  module Of_V1 : CONV with module V_from := V1 and module V_to := V1 = struct
-    let public_key_hash x = x
-
-    let public_key x = x
-
-    let secret_key x = x
-
-    let signature x = x
-  end
+(** Module to define the alias [Signature] as {!Signature.V1}. *)
+module V1 = struct
+  module Signature = Signature.V1
 end
-
-include V_latest
