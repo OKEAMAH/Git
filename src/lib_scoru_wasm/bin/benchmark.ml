65aa1f3991307d4cb55606f1a35771417ebec402
pecornilleau <pe.cornilleau@marigold.dev>
2022-09-23 18:20:03 +0200

    exec_loop

--- src/lib_scoru_wasm/bin/benchmark.ml
+++ src/lib_scoru_wasm/bin/benchmark.ml
@@ -30,6 +30,71 @@ end
 module Tree_encoding_runner = Tree_encoding.Runner.Make (Tree)
 module Wasm = Wasm_pvm.Make (Tree)
 
+module Exec = struct
+  (* let rec eval_until_input_requested tree =
+     let open Lwt_syntax in
+     let* info = Wasm.get_info tree in
+     match info.input_request with
+     | No_input_required ->
+         let* tree =
+           Wasm.Internal_for_tests.compute_step_many
+             ~max_steps:Int64.max_int
+             tree
+         in
+         eval_until_input_requested tree
+     | Input_required -> return tree *)
+
+  let eval_until_input_requested tree =
+    let should_continue (pvm_state : Wasm.pvm_state) =
+      match pvm_state.input_request with
+      | No_input_required -> true
+      | Input_required -> false
+    in
+    Wasm.Internal_for_tests.compute_step_many_until
+      ~max_steps:Int64.max_int
+      should_continue
+      tree
+
+  (** FIXME: very fragile, when the PVM implementation changes, the predicates produced could be way off*)
+  let should_continue phase (pvm_state : Wasm.pvm_state) =
+    match (phase, pvm_state.tick_state) with
+    | `Initialising, Init _ -> true
+    | `Linking, Link _ -> true
+    | `Decoding, Decode _ -> true
+    | ( `Evaluating,
+        Eval {step_kont = Tezos_webassembly_interpreter.Eval.(SK_Result _); _} )
+      ->
+        false
+    | `Evaluating, Eval _ -> true
+    | _, _ -> false
+
+  let finish_top_level_call tree = eval_until_input_requested tree
+
+  let decode tree =
+    Wasm.Internal_for_tests.compute_step_many_until
+      ~max_steps:Int64.max_int
+      (should_continue `Decoding)
+      tree
+
+  let link tree =
+    Wasm.Internal_for_tests.compute_step_many_until
+      ~max_steps:Int64.max_int
+      (should_continue `Linking)
+      tree
+
+  let init tree =
+    Wasm.Internal_for_tests.compute_step_many_until
+      ~max_steps:Int64.max_int
+      (should_continue `Initialising)
+      tree
+
+  let eval tree =
+    Wasm.Internal_for_tests.compute_step_many_until
+      ~max_steps:Int64.max_int
+      (should_continue `Evaluating)
+      tree
+end
+
 let read_message name =
   let open Tezt.Base in
   let kernel_file =
@@ -57,25 +122,6 @@ let initial_boot_sector_from_kernel ?(max_tick = 2_500_000_000) kernel =
     Wasm.Internal_for_tests.set_max_nb_ticks (Z.of_int max_tick) tree
   in
   (context, tree)
-(*
-   let produce_proof context tree kont =
-     let open Lwt_syntax in
-     let* context = Context.add_tree context [] tree in
-     let _hash = Context.commit ~time:Time.Protocol.epoch context in
-     let index = Context.index context in
-     match Context.Tree.kinded_key tree with
-     | Some k -> Context.produce_tree_proof index k kont
-     | None ->
-         Stdlib.failwith
-           "produce_proof: internal error, [kinded_key] returned [None]"
-
-   let proof_size proof =
-     let encoding =
-       Tezos_context_merkle_proof_encoding.Merkle_proof_encoding.V2.Tree2
-       .tree_proof_encoding
-     in
-     let bytes = Data_encoding.Binary.to_bytes_exn encoding proof in
-     Bytes.length bytes *)
 
 let label_step_kont = function
   | Eval.LS_Start _ -> "ls_start"
@@ -120,17 +166,6 @@ let tick_label = function
   | Stuck _ -> "stuck"
   | Link _ -> "link"
 
-let rec eval_until_input_requested tree =
-  let open Lwt_syntax in
-  let* info = Wasm.get_info tree in
-  match info.input_request with
-  | No_input_required ->
-      let* tree =
-        Wasm.Internal_for_tests.compute_step_many ~max_steps:Int64.max_int tree
-      in
-      eval_until_input_requested tree
-  | Input_required -> return tree
-
 let run kernel k =
   let open Lwt_syntax in
   let* () =
@@ -154,12 +189,6 @@ let set_input_step message_counter message tree =
 
 type action = Wasm.tree -> Wasm.tree Lwt.t
 
-let action_from_message step message tree =
-  let open Lwt_syntax in
-  let message = read_message message in
-  let* tree = set_input_step step message tree in
-  eval_until_input_requested tree
-
 type scenario_step = string * action
 
 type scenario = {kernel : string; actions : scenario_step list}
@@ -169,50 +198,77 @@ let make_scenario kernel actions = {kernel; actions}
 let make_action (label : string) (action : action) : scenario_step =
   (label, action)
 
-let run_action name tree action =
+let run_action ?(inline = true) ?(last_tick = false) name tree action =
   let open Lwt_syntax in
+  (* Before *)
   let before = Unix.gettimeofday () in
   let* info = Wasm.get_info tree in
   let before_tick = info.current_tick in
   let _ =
-    Printf.printf
-      "=========\n%s \nStart at tick %s\n%!"
-      name
-      (Z.to_string info.current_tick)
+    if not inline then
+      Printf.printf
+        "=========\n%s \nStart at tick %s\n-----\n%!"
+        name
+        (Z.to_string info.current_tick)
   in
+
+  (* Act *)
   let* tree = action tree in
+
+  (* Result *)
   let time = Unix.gettimeofday () -. before in
   let* info = Wasm.get_info tree in
   let tick = Z.(info.current_tick - before_tick) in
   let* tick_state = Wasm.Internal_for_tests.get_tick_state tree in
-  let _ = Printf.printf "took %s ticks in %f s\n%!" (Z.to_string tick) time in
+  let _ = if not inline then Printf.printf "-----\n" in
   let _ =
-    Printf.printf
-      "last tick: %s %s\n%!"
-      (Z.to_string info.current_tick)
-      (tick_label tick_state)
+    Printf.printf "%s took %s ticks in %f s\n%!" name (Z.to_string tick) time
+  in
+  let _ =
+    if last_tick then
+      Printf.printf
+        "last tick: %s %s\n%!"
+        (Z.to_string info.current_tick)
+        (tick_label tick_state)
   in
   return tree
+
+let exec_loop tree =
+  let open Lwt_syntax in
+  let* tree = run_action "Decoding" tree Exec.decode in
+  let* tree = run_action "Linking" tree Exec.link in
+  let* tree = run_action "Initialisation" tree Exec.init in
+  let* tree = run_action "Evaluation" tree Exec.eval in
+  let* tree = run_action "Finish" tree Exec.finish_top_level_call in
+  return tree
+
+let action_from_message step message tree =
+  let open Lwt_syntax in
+  let message = read_message message in
+  let* tree = set_input_step step message tree in
+  exec_loop tree
 
 let run_scenario scenario =
   let open Lwt_syntax in
   let kernel = scenario.kernel in
-  let aux kernel =
+  let apply_scenario kernel =
     let rec go tree = function
       | [] -> return tree
       | (label, action) :: q ->
-          let* tree = run_action label tree action in
+          let* tree =
+            run_action ~last_tick:true ~inline:false label tree action
+          in
           go tree q
     in
+
     let* _, tree = initial_boot_sector_from_kernel kernel in
     let* tree =
-      run_action "Boot on empty" tree (fun tree ->
-          eval_until_input_requested tree)
+      run_action ~last_tick:true ~inline:false "Booting" tree exec_loop
     in
     let* _ = go tree scenario.actions in
     return ()
   in
-  run kernel aux
+  run kernel apply_scenario
 
 let scenario_tx_kernel =
   make_scenario
@@ -222,9 +278,9 @@ let scenario_tx_kernel =
           let open Lwt_syntax in
           let message = "test" in
           let* tree = set_input_step 1_000 message tree in
-          eval_until_input_requested tree);
+          exec_loop tree);
       make_action "Deposit" (action_from_message 1_001 "deposit");
-      make_action "Withdraw" (action_from_message 1_002 "deposit");
+      make_action "Withdraw" (action_from_message 1_002 "withdrawal");
     ]
 
 let () = Lwt_main.run @@ run_scenario scenario_tx_kernel
