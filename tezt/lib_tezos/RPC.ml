a0baa75ee4758fc222fb02064aad0d778346877c
Rodi-Can Bozman <rbozman.functori@gmail.com>
2022-12-05 16:42:17 +0100

    Proto, Scoru: fix ongoing_refutation_games service
    (cherry picked from commit f282c3971fe174dedf6cc4c364fdc963ca814e90)

--- tezt/lib_tezos/RPC.ml
+++ tezt/lib_tezos/RPC.ml
@@ -750,11 +750,10 @@ let get_chain_block_context_sc_rollups_all ?(chain = "main") ?(block = "head")
     ["chains"; chain; "blocks"; block; "context"; "sc_rollups"; "all"]
     Fun.id
 
-let get_chain_block_context_sc_rollups_sc_rollup_games ?(chain = "main")
-    ?(block = "head") ~staker sc_rollup () =
+let get_chain_block_context_sc_rollups_sc_rollup_games_with_staker
+    ?(chain = "main") ?(block = "head") ~staker sc_rollup () =
   make
     GET
-    ~query_string:[("staker", staker)]
     [
       "chains";
       chain;
@@ -764,7 +763,8 @@ let get_chain_block_context_sc_rollups_sc_rollup_games ?(chain = "main")
       "sc_rollups";
       "sc_rollup";
       sc_rollup;
-      "games";
+      "games_with_staker";
+      staker;
     ]
     Fun.id
 
