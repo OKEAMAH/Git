668ae38c613b279b66f0981a9a09898fa07524dd
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2023-01-04 14:27:52 +0100

    tezt: rename smart rollup constants
    (cherry picked from commit 79fee8dda741c53fd840508d4612bfc3163ca48d)

--- tezt/tests/dal.ml
+++ tezt/tests/dal.ml
@@ -97,18 +97,15 @@ let with_layer1 ?(attestation_lag = 1) ?commitment_period ?challenge_window
       ["dal_parametric"; "attestation_lag"]
       (Some attestation_lag)
     @ make_int_parameter
-        ["sc_rollup_commitment_period_in_blocks"]
+        ["smart_rollup_commitment_period_in_blocks"]
         commitment_period
     @ make_int_parameter
-        ["sc_rollup_challenge_window_in_blocks"]
+        ["smart_rollup_challenge_window_in_blocks"]
         challenge_window
     (* this will produce the empty list if dal_enable is not passed to the function invocation,
        hence the value from the protocol constants will be used. *)
     @ dal_enable_param dal_enable
-    @ [
-        (["sc_rollup_enable"], `Bool true);
-        (["sc_rollup_arith_pvm_enable"], `Bool true);
-      ]
+    @ [(["smart_rollup_arith_pvm_enable"], `Bool true)]
   in
   let base = Either.right (protocol, None) in
   let* parameter_file = Protocol.write_parameter_file ~base parameters in
