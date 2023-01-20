34e201c237fdb952ac1f8e04194e72692929dbb7
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-01-20 14:54:37 +0100

    Tezt/Records: extract [Gitlab] module

---
+++ tezt/records/gitlab.ml
@@ -0,0 +1,57 @@
+open Tezt
+open Base
+
+let make path =
+  Uri.make ~scheme:"https" ~host:"gitlab.com" ~path:("api/v4/" ^ path)
+
+let query_opt name value_opt =
+  match value_opt with None -> [] | Some value -> [(name, [value])]
+
+let project_pipelines ~project ?source ?ref_ ?sha ?status =
+  make
+    (sf "projects/%s/pipelines/" (Uri.pct_encode project))
+    ~query:
+      (query_opt "ref" ref_ @ query_opt "source" source @ query_opt "sha" sha
+     @ query_opt "status" status)
+
+let project_pipeline_jobs ~project ~pipeline =
+  make (sf "projects/%s/pipelines/%d/jobs" (Uri.pct_encode project) pipeline)
+
+let project_commits ~project ?ref_name =
+  make
+    (sf "projects/%s/repository/commits" (Uri.pct_encode project))
+    ~query:(query_opt "ref_name" ref_name)
+
+let project_job_artifact ~project ~job_id ~artifact_path =
+  make
+    (sf
+       "projects/%s/jobs/%d/artifacts/%s"
+       (Uri.pct_encode project)
+       job_id
+       artifact_path)
+
+let project_job_artifacts ~project ~job_id =
+  make (sf "projects/%s/jobs/%d/artifacts" (Uri.pct_encode project) job_id)
+
+let get ?(curl_args = []) uri =
+  let url = Uri.to_string uri in
+  let* raw_json = Process.run_and_read_stdout "curl" (curl_args @ [url]) in
+  return (JSON.parse ~origin:url raw_json)
+
+let get_output ~output_path uri =
+  let url = Uri.to_string uri in
+  Process.run "curl" ["--location"; url; "--output"; output_path]
+
+let get_all uri =
+  let url = Uri.to_string uri in
+  let rec aux from acc =
+    (* GitLab uses a lot of redirections so we use --location to follow them. *)
+    let full_url = url ^ "?per_page=100&page=" ^ string_of_int from in
+    let* response_body = Process.run_and_read_stdout "curl" [full_url] in
+    let list = JSON.parse ~origin:url response_body |> JSON.as_list in
+    Log.info "Found %d items in page %d of %s." (List.length list) from url ;
+    match list with
+    | [] -> return (List.rev acc)
+    | _ :: _ -> aux (from + 1) (List.rev_append list acc)
+  in
+  aux 1 []
--- tezt/records/update.ml
+++ tezt/records/update.ml
@@ -35,63 +35,6 @@ let project = Sys.getenv_opt "PROJECT" |> Option.value ~default:"tezos/tezos"
 let default_branch =
   Sys.getenv_opt "DEFAULT_BRANCH" |> Option.value ~default:"master"
 
-module Gitlab = struct
-  let make path =
-    Uri.make ~scheme:"https" ~host:"gitlab.com" ~path:("api/v4/" ^ path)
-
-  let query_opt name value_opt =
-    match value_opt with None -> [] | Some value -> [(name, [value])]
-
-  let project_pipelines ?source ?ref_ ?sha ?status =
-    make
-      (sf "projects/%s/pipelines/" (Uri.pct_encode project))
-      ~query:
-        (query_opt "ref" ref_ @ query_opt "source" source @ query_opt "sha" sha
-       @ query_opt "status" status)
-
-  let project_pipeline_jobs ~pipeline =
-    make (sf "projects/%s/pipelines/%d/jobs" (Uri.pct_encode project) pipeline)
-
-  let project_commits ?ref_name =
-    make
-      (sf "projects/%s/repository/commits" (Uri.pct_encode project))
-      ~query:(query_opt "ref_name" ref_name)
-
-  let project_job_artifact ~job_id ~artifact_path =
-    make
-      (sf
-         "projects/%s/jobs/%d/artifacts/%s"
-         (Uri.pct_encode project)
-         job_id
-         artifact_path)
-
-  let project_job_artifacts ~job_id =
-    make (sf "projects/%s/jobs/%d/artifacts" (Uri.pct_encode project) job_id)
-
-  let get uri =
-    let url = Uri.to_string uri in
-    let* raw_json = Process.run_and_read_stdout "curl" [url] in
-    return (JSON.parse ~origin:url raw_json)
-
-  let get_output ~output_path uri =
-    let url = Uri.to_string uri in
-    Process.run "curl" ["--location"; url; "--output"; output_path]
-
-  let get_all uri =
-    let url = Uri.to_string uri in
-    let rec aux from acc =
-      (* GitLab uses a lot of redirections so we use --location to follow them. *)
-      let full_url = url ^ "?per_page=100&page=" ^ string_of_int from in
-      let* response_body = Process.run_and_read_stdout "curl" [full_url] in
-      let list = JSON.parse ~origin:url response_body |> JSON.as_list in
-      Log.info "Found %d items in page %d of %s." (List.length list) from url ;
-      match list with
-      | [] -> return (List.rev acc)
-      | _ :: _ -> aux (from + 1) (List.rev_append list acc)
-    in
-    aux 1 []
-end
-
 let records_directory = "tezt/records"
 
 let fetch_record (uri, index) =
@@ -118,7 +61,7 @@ let remove_existing_records new_records =
 
 let fetch_pipeline_records_from_jobs pipeline =
   Log.info "Fetching records from tezt executions in %d in %s" pipeline project ;
-  let* jobs = Gitlab.(project_pipeline_jobs ~pipeline () |> get_all) in
+  let* jobs = Gitlab.(project_pipeline_jobs ~project ~pipeline () |> get_all) in
   let get_record job =
     let job_id = JSON.(job |-> "id" |> as_int) in
     let name = JSON.(job |-> "name" |> as_string) in
@@ -127,6 +70,7 @@ let fetch_pipeline_records_from_jobs pipeline =
     | Some index ->
         Some
           ( Gitlab.project_job_artifact
+              ~project
               ~job_id
               ~artifact_path:("tezt-results-" ^ index ^ ".json")
               (),
@@ -143,7 +87,7 @@ let fetch_pipeline_records_from_job pipeline ~job_name ~artifact_folder =
     job_name
     pipeline
     project ;
-  let* jobs = Gitlab.(project_pipeline_jobs ~pipeline () |> get_all) in
+  let* jobs = Gitlab.(project_pipeline_jobs ~project ~pipeline () |> get_all) in
   let job_opt =
     List.find_opt
       (fun job ->
@@ -158,7 +102,8 @@ let fetch_pipeline_records_from_job pipeline ~job_name ~artifact_folder =
       let output_path = Temp.file (output_base ^ ".zip") in
       let job_id = JSON.(job |-> "id" |> as_int) in
       let* () =
-        Gitlab.(get_output ~output_path @@ project_job_artifacts ~job_id ())
+        Gitlab.(
+          get_output ~output_path @@ project_job_artifacts ~project ~job_id ())
       in
       let tmp = Temp.dir output_base in
       let* () =
@@ -206,7 +151,9 @@ let get_last_merged_pipeline () =
     project
     default_branch ;
   let commit_hash commit = JSON.(commit |-> "id" |> as_string) in
-  let* commits = Gitlab.(project_commits ~ref_name:default_branch () |> get) in
+  let* commits =
+    Gitlab.(project_commits ~project ~ref_name:default_branch () |> get)
+  in
   let commits = JSON.as_list commits in
   let rec aux = function
     | [] | [_] ->
@@ -219,7 +166,8 @@ let get_last_merged_pipeline () =
           Log.info "%s is a merge commit parent" (commit_hash commit_parent) ;
           let* pipelines =
             Gitlab.(
-              project_pipelines ~sha:(commit_hash commit_parent) () |> get)
+              project_pipelines ~project ~sha:(commit_hash commit_parent) ()
+              |> get)
           in
           match JSON.as_list pipelines with
           | pipeline :: _ ->
@@ -242,7 +190,12 @@ let get_last_default_branch_push_pipeline () =
   (* Returns the ID of the last push-pipeline on the default branch *)
   let* pipelines =
     Gitlab.(
-      project_pipelines ~status:"success" ~source:"push" ~ref_:default_branch ()
+      project_pipelines
+        ~project
+        ~status:"success"
+        ~source:"push"
+        ~ref_:default_branch
+        ()
       |> get)
   in
   return JSON.(pipelines |=> 0 |-> "id" |> as_int)
