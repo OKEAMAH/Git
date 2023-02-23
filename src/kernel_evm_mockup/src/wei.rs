f6376f56cf645e96004300953a3ede5e6bb2b14c
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2023-02-23 12:29:53 +0000

    EVM/Mockup: improve Wei implementation
    Proper newtype now, to prevent mixing it with the underlying type.

--- src/kernel_evm_mockup/src/wei.rs
+++ src/kernel_evm_mockup/src/wei.rs
@@ -4,10 +4,69 @@
 
 use num_bigint::BigUint;
 
-pub type Wei = BigUint;
+use rlp::{Decodable, DecoderError, Rlp};
+use std::ops::{Add, Deref, Mul, Sub};
+
+pub struct Wei(BigUint);
 
 pub const ETH_AS_WEI: u64 = 1_000_000_000_000_000_000;
 
+impl Deref for Wei {
+    type Target = BigUint;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Wei {
+    pub fn from_bytes_le(bytes: &[u8]) -> Wei {
+        Wei(BigUint::from_bytes_le(bytes))
+    }
+}
+
+impl From<u64> for Wei {
+    fn from(v: u64) -> Wei {
+        Wei(BigUint::from(v))
+    }
+}
+
+impl From<BigUint> for Wei {
+    fn from(b: BigUint) -> Wei {
+        Wei(b)
+    }
+}
+
+impl Add for Wei {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Wei(self.0 + other.0)
+    }
+}
+
+impl Sub for Wei {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Wei(self.0 - other.0)
+    }
+}
+
+impl Mul for Wei {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Wei(self.0 * other.0)
+    }
+}
+
 pub fn from_eth(eth: u64) -> Wei {
     Wei::from(eth) * Wei::from(ETH_AS_WEI)
+}
+
+impl Decodable for Wei {
+    fn decode(decoder: &Rlp<'_>) -> Result<Wei, DecoderError> {
+        Ok(Wei::from_bytes_le(decoder.data()?))
+    }
 }
