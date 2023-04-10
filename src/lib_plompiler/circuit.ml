5e7ec36e933d51546a16c15977260b8590ad755f
ambrona <mac.ambrona@gmail.com>
2023-04-10 16:20:18 +0200

    lib_plompiler solver circuit: generalize [wires_desc]

--- src/lib_plompiler/circuit.ml
+++ src/lib_plompiler/circuit.ml
@@ -543,7 +543,7 @@ module Num = struct
              "is_zero";
          |]
        in
-       let solver = IsZero {a = l; b = r; c = bit; d = 0; e = 0} in
+       let solver = IsZero [|l; r; bit|] in
        append gate ~solver >* assert_nonzero (Scalar r) >* ret @@ Bool bit
 
   let is_not_zero (Scalar l) =
@@ -561,7 +561,7 @@ module Num = struct
              "is_not_zero";
          |]
        in
-       let solver = IsNotZero {a = l; b = r; c = bit; d = 0; e = 0} in
+       let solver = IsNotZero [|l; r; bit|] in
        append gate ~solver >* assert_nonzero (Scalar r) >* ret @@ Bool bit
 
   let assert_bool (Scalar l) =
