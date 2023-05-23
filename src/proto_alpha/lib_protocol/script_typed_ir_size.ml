6d65bb7b5fa49152363efe68e6046a3c7adc03ae
Marco Stronati <marco@stronati.org>
2023-05-23 15:02:26 +0200

    Proto/Script_size: remove rsa_public from chest

--- src/proto_alpha/lib_protocol/script_typed_ir_size.ml
+++ src/proto_alpha/lib_protocol/script_typed_ir_size.ml
@@ -230,14 +230,12 @@ let chest_size chest =
   (*
      type chest = {
        puzzle : puzzle;
-       rsa_public : rsa_public;
        ciphertext : ciphertext;
      }
   *)
   let puzzle_size = 256 in
-  let rsa_public_size = 256 in
   let ciphertext_size = Script_timelock.get_plaintext_size chest in
-  h3w +? (puzzle_size + rsa_public_size + ciphertext_size)
+  h3w +? (puzzle_size + ciphertext_size)
 
 let chest_key_size _ =
   (*
