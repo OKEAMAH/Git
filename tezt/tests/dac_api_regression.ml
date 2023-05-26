53971f2e642b084c0c814caf04a8cd047aedcfc7
Martin Tomazic <martin.tomazic97@gmail.com>
2023-05-26 14:12:19 +0200

    DAC/tezt: Regression test for "POST v0/preimage"

--- tezt/tests/dac_api_regression.ml
+++ tezt/tests/dac_api_regression.ml
@@ -32,9 +32,44 @@
 
 open Dac_helper
 
+let make ?data ?query_string =
+  RPC.make
+    ?data
+    ?query_string
+    ~get_host:Dac_node.rpc_host
+    ~get_port:Dac_node.rpc_port
+    ~get_scheme:(Fun.const "http")
+
 (** [V0] module is used for regression testing [V0] API. *)
 module V0 = struct
-  let test_dummy _ = Lwt.return_unit
+  let encode_bytes_to_hex_string raw =
+    "\"" ^ match Hex.of_string raw with `Hex s -> s ^ "\""
+
+  (** [post_preimage] asserts the binding contract of "POST v0/preimage"
+      request. *)
+  let post_preimage =
+    (* [post_preimage_request] shape is binding. *)
+    let post_preimage_request =
+      JSON.parse
+        ~origin:"Dac_api_regression.V0.coordinator_post_preimage"
+        (encode_bytes_to_hex_string "test")
+    in
+    let data : RPC_core.data = Data (JSON.unannotate post_preimage_request) in
+    make ~data POST [Dac_rpc.V0.api_prefix; "preimage"] Fun.id
+
+  (** [assert_post_preimage_reponse] asserts the binding contract of
+      "POST v0/preimage response". *)
+  let assert_post_preimage_response reponse =
+    let _root_hash = JSON.as_string reponse in
+    Lwt.return_unit
+
+  (** [test_coordinator_post_preimage] tests Cooordinator's
+      "POST v0/preimage". *)
+  let test_coordinator_post_preimage Scenarios.{coordinator_node; _} =
+    (* 1. Test binding contract of RPC request.
+       2. Test binding contract of RPC response. *)
+    let* response = RPC.call coordinator_node post_preimage in
+    assert_post_preimage_response response
 end
 
 let register ~protocols =
@@ -43,6 +78,6 @@ let register ~protocols =
     ~observers:0
     ~committee_size:0
     ~tags:["dac"; "dac_node"; "api_regression"]
-    "test dummy"
-    V0.test_dummy
+    "test Coordinator's post preimage"
+    V0.test_coordinator_post_preimage
     protocols
