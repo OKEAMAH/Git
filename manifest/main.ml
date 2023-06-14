58ac3e86081ec927eeef3ae93d0bae00cbbe6551
Ole Krüger <ole.kruger@trili.tech>
2023-06-14 15:02:36 +0100

    Build: Link Oxford

--- manifest/main.ml
+++ manifest/main.ml
@@ -6501,6 +6501,8 @@ let hash = Protocol.hash
 
   let _017_PtNairob = active (Name.v "PtNairob" 017)
 
+  let _018_Proxford = active (Name.v "Proxford" 018)
+
   let alpha = active Name.alpha
 
   let all = List.rev !all_rev
