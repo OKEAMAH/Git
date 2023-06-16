8dad8307906e4af8c9c621a50986d16a895a7609
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-06-16 10:46:47 +0200

    instrument doc_publish.sh

--- scripts/ci/doc_publish.sh
+++ scripts/ci/doc_publish.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 
+set -x
+
 cd "${CI_PROJECT_DIR}" || exit 1
 
-make all ;
-make -C docs -j full ;
+echo "State after unzipping cache:"
+[ -d  _build/default/_doc/_html/tezos-base/Tezos_base/ ] &&
+    ls _build/default/_doc/_html/tezos-base/Tezos_base/
 
-if [ "${CI_COMMIT_REF_NAME}" == "master" ] ; then
-  git clone --depth 5 git@gitlab.com:"${CI_PROJECT_NAMESPACE}"/"${CI_PROJECT_NAMESPACE}".gitlab.io gitlab.io ;
-  rsync --recursive --links --perms --delete --exclude=.doctrees --exclude={{main,alpha,zero}net,master}/index.html docs/_build/ gitlab.io/public/ ;
-  cd gitlab.io || exit 2;
+make all
+dune build @doc > odoc.log 2>&1
+[ -f odoc.log ] && head odoc.log
 
-  if [ -z "$(git status -s)" ] ; then
-      echo "Nothing to commit!" ;
-  else
-      git add public ;
-      git commit -m "Import doc of ${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}:${CI_COMMIT_SHA}" ;
-      git push origin master ;
-  fi
-else
-  echo "Skip pushing documentation. Only pushing for real master" ;
-fi
+echo "State 'dune build @doc':"
+[ -d  _build/default/_doc/_html/tezos-base/Tezos_base/ ] &&
+    ls _build/default/_doc/_html/tezos-base/Tezos_base/
