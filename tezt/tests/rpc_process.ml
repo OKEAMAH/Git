0810d066163efdf5d1f6b567eb42c992a5e43b7a
Victor Allombert <victor.allombert@nomadic-labs.com>
2023-07-13 15:08:10 +0200

    fixup ?? Tezt: add new test

--- tezt/tests/rpc_process.ml
+++ tezt/tests/rpc_process.ml
@@ -89,4 +89,51 @@ let test_kill =
   let* (_ : bool) = head_can_be_requested ~expected_level:1 client in
   unit
 
-let register ~protocols = test_kill protocols
+let wait_for_RPC_process_sync node =
+  let filter json = JSON.(json |> as_int_opt) in
+  Node.wait_for node "synchronized.v0" filter
+
+let wait_for_local_rpc_server node =
+  Node.wait_for node "starting_local_rpc_server.v0" (fun _ -> Some ())
+
+let test_sync_with_node =
+  Protocol.register_test
+    ~__FILE__
+    ~title:"RPC is sync with node"
+    ~tags:["rpc"; "process"; "node"; "sync"]
+  @@ fun protocol ->
+  let node_arguments = Node.[Synchronisation_threshold 0] in
+  (* Default node running with the RPC_process *)
+  let* node_rpc_process = Node.init ~name:"node_rpc_process" node_arguments in
+  let* () = Node.wait_for_ready node_rpc_process in
+  let node_local_rpc =
+    Node.create ~local_rpc_server:true ~name:"node_local_rpc" node_arguments
+  in
+  let waiter_for_local_rpc_server = wait_for_local_rpc_server node_local_rpc in
+  let* () = Node.run node_local_rpc node_arguments in
+  let* () = Node.wait_for_ready node_local_rpc in
+  (* Wait for the event that states that the local RPC server is
+     used. *)
+  let* () = waiter_for_local_rpc_server in
+  let* client = Client.init ~endpoint:(Node node_rpc_process) () in
+  let* () = Client.Admin.connect_address ~peer:node_local_rpc client in
+  let waiter_for_RPC_process_sync =
+    wait_for_RPC_process_sync node_rpc_process
+  in
+  let* () = Client.activate_protocol_and_wait ~protocol client in
+  Log.info
+    "Wait for RPC_process sync at level 1 on the %s"
+    (Node.name node_rpc_process) ;
+  let* (_ : int) = waiter_for_RPC_process_sync in
+  Log.info "Wait for level 1 on the %s" (Node.name node_rpc_process) ;
+  (* Node.wait_for_level relies on the RPC_process synchronization
+     event.*)
+  let* (_ : int) = Node.wait_for_level node_rpc_process 1 in
+  (* Node.wait_for_level relies on the set_head/branch_switch
+     events. *)
+  let* (_ : int) = Node.wait_for_level node_local_rpc 1 in
+  unit
+
+let register ~protocols =
+  test_kill protocols ;
+  test_sync_with_node protocols
