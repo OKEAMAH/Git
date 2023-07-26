8aeaa7bd87019b22f8649e2c995a6f1a580b810e
vbot <vincent.botbol@nomadic-labs.com>
2023-07-26 14:26:50 +0200

    plug requester profiler

--- manifest/main.ml
+++ manifest/main.ml
@@ -3421,7 +3421,7 @@ let _octez_store_mocked =
     ~implements:octez_store
 
 let octez_requester =
-  octez_lib
+  octez_shell_lib
     "requester"
     ~internal_name:"tezos_requester"
     ~path:"src/lib_requester"
@@ -3429,6 +3429,7 @@ let octez_requester =
       [
         octez_base |> open_ ~m:"TzPervasives";
         octez_stdlib_unix |> open_;
+        octez_shell_services |> open_;
         lwt_watcher;
       ]
 
@@ -3436,7 +3437,7 @@ let _octez_requester_tests =
   tezt
     ["test_requester"; "test_fuzzing_requester"; "shared"]
     ~path:"src/lib_requester/test"
-    ~opam:"octez-libs"
+    ~opam:"octez-shell-libs"
     ~deps:
       [
         octez_base |> open_ ~m:"TzPervasives";
