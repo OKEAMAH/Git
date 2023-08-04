d8dabeac00ee074b2e5cbf6d0e037f28cfea780f
lin <linoshitani@gmail.com>
2023-08-04 17:38:34 +0800

    Dal/Infra: Copy-and-paste tf-gcp-state-storage-bucket

---
+++ devtools/cloud-infrastructure/projects/nl-dal/prerequisite/state-storage-bucket/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the bucket"
+  type        = string
+}
