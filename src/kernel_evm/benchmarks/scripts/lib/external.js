9586f3688dda0162b3b5f8ad3eeea1d9f68f0f67
pecornilleau <pe.cornilleau@marigold.dev>
2023-08-10 19:42:25 +0200

    EVM: Bench: remove hardcoded paths

---
+++ src/kernel_evm/benchmarks/scripts/lib/external.js
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2023 Marigold <contact@marigold.dev>
+//
+// SPDX-License-Identifier: MIT
+
+const path = require('node:path');
+
+const in_prod = function () {
+    return process.env.NODE_ENV == "production"
+}
+
+const bin = function (filepath) {
+    if (in_prod()) {
+        return path.basename(filepath)
+    } else {
+        return filepath
+    }
+}
+
+const ressource = function (filepath) {
+    if (in_prod() && process.env.EXTERNAL_RESSOURCES) {
+        return path.format({
+            dir: process.env.EXTERNAL_RESSOURCES,
+            base: path.basename(filepath)
+        })
+    } else {
+        return filepath
+    }
+}
+
+module.exports = { bin, ressource }
