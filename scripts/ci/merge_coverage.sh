7d6ac255062f6fa232f15fdfc41961d9a466d551
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-08-30 17:35:11 +0200

    CI: Rework corrupt coverage reporting
    Instead of failing the job where corrupt coverage was produced, we
    track failing coverage and the details thereof in a JSON file
    artifact. Corrupt coverage is summarized and reported in the
    [unified_coverage] job instead.

--- scripts/ci/merge_coverage.sh
+++ scripts/ci/merge_coverage.sh
@@ -6,21 +6,27 @@ COVERAGE_MERGED=$(echo "$CI_JOB_NAME" | tr --squeeze-repeats '[\/_ @[]+' '-')
 
 # If the ci--no-coverage label is set, we do not attempt to merge the coverage files
 if echo "${CI_MERGE_REQUEST_LABELS:-}" | grep -q '\(^\|,\)ci--no-coverage\($\|,\)' ; then
-    rm "$BISECT_FILE"*.coverage || true
+    rm -f "$BISECT_FILE"*.coverage || true
     echo "Coverage is disabled."
 elif bisect-ppx-report merge --coverage-path "$BISECT_FILE" "$COVERAGE_MERGED".coverage; then
     # Merge was successful, meaning that no corrupted files were found
     COVERAGE_MERGED="$COVERAGE_MERGED".coverage
-    rm "$BISECT_FILE"*.coverage || true
+    rm -f "$BISECT_FILE"*.coverage || true
     mv "$COVERAGE_MERGED" "$BISECT_FILE"
     echo "Merged coverage files to ${BISECT_FILE}/${COVERAGE_MERGED}"
 else
-    # Merge was not successful, meaning that coverage was corrupted
-    rm "$BISECT_FILE"*.coverage || true
-    echo "Corrupted coverage files were found, please report this in https://gitlab.com/tezos/tezos/-/issues/1529";
-    if [ "${SLACK_COVERAGE_TOKEN:-}" != "" ]; then
-        scripts/send_slack_alert_coverage.sh "$SLACK_COVERAGE_TOKEN" "$SLACK_COVERAGE_CHANNEL";
-    fi
-    exit 64
+    # Merge was not successful, meaning that coverage was corrupted.
+    # Store failure details as an artifact that is treated in the job
+    # `unified_coverage`.
+    rm -f "$BISECT_FILE"*.coverage || true
+    COVERAGE_MERGED="$COVERAGE_MERGED".corrupt.json
+    jq -n \
+       --arg job_id "$CI_JOB_ID" \
+       --arg job_name "$CI_JOB_NAME" \
+       --arg job_web_url "$CI_JOB_URL" \
+       '{"job_id": $job_id, "job_name": $job_name, "job_web_url": $job_web_url}' \
+       > "$COVERAGE_MERGED"
+    mv "$COVERAGE_MERGED" "$BISECT_FILE"
+    echo "Corrupt coverage files detected, wrote ${BISECT_FILE}${COVERAGE_MERGED}"
 fi
 
--- scripts/ci/report_coverage.sh
+++ scripts/ci/report_coverage.sh
@@ -2,8 +2,36 @@
 
 set -eu
 
-# Compute coverage
-make coverage-report
-# Rewrite the summary output to remove information points matching the coverage regexp below
-make coverage-report-summary | sed 's@Coverage: [[:digit:]]\+/[[:digit:]]\+ (\(.*%\))@Coverage: \1@'
-make coverage-report-cobertura
+if [ -z "$(find "$BISECT_FILE" -maxdepth 1 -name '*.corrupt.json' -print -quit)" ]; then
+    # Compute coverage
+    make coverage-report
+    # Rewrite the summary output to remove information points matching the coverage regexp below
+    make coverage-report-summary | sed 's@Coverage: [[:digit:]]\+/[[:digit:]]\+ (\(.*%\))@Coverage: \1@'
+    make coverage-report-cobertura
+    exit 0
+else
+    # Corrupt coverage files were detected
+    echo "Corrupted coverage files were found, please report this in https://gitlab.com/tezos/tezos/-/issues/1529";
+    slack_msg=$(
+        echo "⚠️ Corrupted coverage file(s) found in pipeline <$CI_PIPELINE_URL|#$CI_PIPELINE_ID> ⚠️"
+        echo
+        find "$BISECT_FILE" -maxdepth 1 -name '*.corrupt.json' -exec \
+             jq -r '"  • Job <" + .job_web_url + "|#" + .job_id + "> (`" + .job_name + "`)"' \{\} \;
+    )
+    if [ "${SLACK_COVERAGE_TOKEN:-}" != "" ]; then
+        # :
+        curl --silent \
+             -H "Authorization: Bearer $SLACK_COVERAGE_TOKEN" \
+             -d "channel=$CHANNEL_ID" \
+             -d "text=${slack_msg}" \
+             -X POST https://slack.com/api/chat.postMessage -o slack-response.json
+        jq --exit-status '.ok' < slack-response.json > /dev/null || {
+            echo "Slack notification unsuccessful, response received:";
+            cat slack-response.json;
+            exit 1;
+        }
+        echo "Slack notification posted to channel $CHANNEL_ID"
+    fi
+
+    exit 64
+fi
