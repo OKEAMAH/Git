db77eb74c87b88bd3a53b2bd1c1e939351ef58cf
Rémy El Sibaïe <remy.el-sibaie@nomadic-labs.com>
2023-09-01 15:20:44 +0200

    rpc_process: simplify and extend monitoring
    - handle applied_blocks
    - use monitored heads rpc to feed on proxied head rpc instead of accessing store

--- src/lib_rpc_process/directory.ml
+++ src/lib_rpc_process/directory.ml
@@ -24,25 +24,55 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let monitor_head dir (store : Store.t option ref)
-    (head_watcher : (Block_hash.t * Block_header.t) Lwt_watcher.input) =
+let reproduce_stream watcher =
+  let stream, stopper = watcher in
+  let shutdown () = Lwt_watcher.shutdown stopper in
+  let next () = Lwt_stream.get stream in
+  Tezos_rpc.Answer.return_stream {next; shutdown}
+
+let reproduce_head_stream get_last_head watcher =
+  let stream, stopper = watcher in
+  let shutdown () = Lwt_watcher.shutdown stopper in
+  let first_call = ref true in
+  let next () =
+    if !first_call then (
+      first_call := false ;
+      Lwt.return @@ get_last_head ())
+    else Lwt_stream.get stream
+  in
+  Tezos_rpc.Answer.return_stream {next; shutdown}
+
+let monitor_dir dir (store : Store.t option ref) get_last_head
+    (head_watcher : (Block_hash.t * Block_header.t) Lwt_watcher.input)
+    block_watcher =
   let dir = ref dir in
   let gen_register1 s f =
     dir := Tezos_rpc.Directory.gen_register !dir s (fun ((), a) p q -> f a p q)
   in
-  gen_register1 Tezos_shell_services.Monitor_services.S.heads (fun chain q () ->
+  let gen_register0 s f =
+    dir := Tezos_rpc.Directory.gen_register !dir s (fun () p q -> f p q)
+  in
+  gen_register0
+    Tezos_shell_services.Monitor_services.S.applied_blocks
+    (fun _q () ->
+      let block_stream = Lwt_watcher.create_stream block_watcher in
+      reproduce_stream block_stream) ;
+  gen_register1
+    Tezos_shell_services.Monitor_services.S.heads
+    (fun _chain _q () ->
       let open Lwt_syntax in
       let head_watcher = Lwt_watcher.create_stream head_watcher in
-      let* store =
+      let* _store =
         match !store with
         | Some store -> return store
         | None -> Lwt.fail Not_found
       in
-      Tezos_shell.Monitor_directory.monitor_head ~head_watcher store chain q ()) ;
+      reproduce_head_stream get_last_head head_watcher) ;
   !dir
 
-let build_rpc_directory node_version config dynamic_store
-    ~(head_watcher : (Block_hash.t * Block_header.t) Lwt_watcher.input) =
+let build_rpc_directory get_last_head node_version config dynamic_store
+    ~(head_watcher : (Block_hash.t * Block_header.t) Lwt_watcher.input)
+    ~applied_blocks_watcher =
   let static_dir = Tezos_shell.Version_directory.rpc_directory node_version in
   let static_dir =
     Tezos_shell.Config_directory.build_rpc_directory_for_rpc_process
@@ -57,7 +87,14 @@ let build_rpc_directory node_version config dynamic_store
     Tezos_rpc.Directory.register0 static_dir Services.config (fun () () ->
         Lwt.return_ok config)
   in
-  let static_dir = monitor_head static_dir dynamic_store head_watcher in
+  let static_dir =
+    monitor_dir
+      static_dir
+      dynamic_store
+      get_last_head
+      head_watcher
+      applied_blocks_watcher
+  in
   Tezos_rpc.Directory.register_dynamic_directory
     static_dir
     (Tezos_rpc.Path.subst1 Tezos_shell_services.Chain_services.path)
