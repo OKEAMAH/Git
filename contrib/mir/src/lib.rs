e1a3171b120162bf3c601bf501fef9b6bb6af40b
martoon <martoon.hsk@gmail.com>
2023-09-06 19:08:31 +0300

    MIR: Add rollup base
    Add a trivial rollup kernel and infra around it.
    
    This includes:
    1. Toy `lib.rs` from tutorials.
    2. Adding `.cargo/config.toml` with wasm as default target.
       We always want to build like that because having wasm target affects
       build - e.g. functionality that includes use of environmental
       randomness is not possible here for reason (rollup should be
       deterministic as it is executed on different nodes).
    3. At this point you still need to pre-install the wasm toolchain.
       `rust-toolchain.toml` lifts that necessity, so that the toolchain is
       automatically installed with `cargo build`.
    4. Declaring `[lib]` in `Cargo.toml`. I also specified `[[bin]]` section
       to avoid the resulting name collisions (two `mir` targets).

---
+++ contrib/mir/src/lib.rs
@@ -0,0 +1,15 @@
+/******************************************************************************/
+/*                                                                            */
+/* SPDX-License-Identifier: MIT                                               */
+/* Copyright (c) [2023] Serokell <hi@serokell.io>                             */
+/*                                                                            */
+/******************************************************************************/
+
+use tezos_smart_rollup::kernel_entry;
+use tezos_smart_rollup::prelude::{debug_msg, Runtime};
+
+kernel_entry!(kernel_entry);
+
+pub fn kernel_entry<Host: Runtime>(host: &mut Host) {
+    debug_msg!(host, "Hello Kernel");
+}
