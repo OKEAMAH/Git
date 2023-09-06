e3fd5a044fd8fc329cd168eef0c3489330dd2ce8
martoon <martoon.hsk@gmail.com>
2023-09-06 23:35:52 +0300

    Add messages parsing

--- contrib/mir/src/rollup/kernel.rs
+++ contrib/mir/src/rollup/kernel.rs
@@ -1,9 +1,14 @@
 use tezos_smart_rollup::{
+    inbox::{InboxMessage, InternalInboxMessage},
     prelude::{debug_msg, Runtime},
     types::Message,
 };
 
+use super::types::*;
+
 // This module follows the reference examples: https://gitlab.com/tezos/kernel-gallery
+
+pub type Error = String;
 
 /// The root method of the kernel.
 ///
@@ -23,8 +28,11 @@ pub fn kernel_entry(host: &mut impl Runtime) {
                     first_message_for_invocation = false;
                 }
 
-                // TODO: consider catching panics
-                process_message(host, &msg);
+                // [To discuss]: consider catching panics per message if possible?
+                let res = process_message(host, &msg);
+                res.unwrap_or_else(|err| {
+                    debug_msg!(host, "Processing message #{} failed: {}", msg.id, err)
+                })
             }
             Err(_) => continue,
             Ok(None) => break,
@@ -32,6 +40,31 @@ pub fn kernel_entry(host: &mut impl Runtime) {
     }
 }
 
-pub fn process_message(host: &mut impl Runtime, msg: &Message) {
-    let _ = (host, msg);
+pub fn process_message(host: &mut impl Runtime, msg: &Message) -> Result<(), Error> {
+    let msg_id = msg.id;
+    let (rest, msg) =
+        InboxMessage::<IncomingTransferParam>::parse(msg.as_ref()).map_err(|x| x.to_string())?;
+    // Likely we don't want to restrict the unparsed input for the sake of
+    // forward compatibility. And the reference kernels do the same thing.
+    debug_assert!(rest.is_empty());
+    match msg {
+        InboxMessage::External(payload) => {
+            debug_msg!(host, "Message #{msg_id} - external: {payload:#x?}")
+        }
+        // [optimization] If payload is bytes, it should not be hard
+        // to avoid copying payload when parsing if we use our own structures.
+        // If it is not necessarily bytes, Nom lib still supports returning borrowed
+        // data and for concrete small type we won't need to write much of a
+        // decoding logic.
+        InboxMessage::Internal(InternalInboxMessage::Transfer(transfer)) => {
+            debug_msg!(
+                host,
+                "Message #{msg_id} - internal transfer to {} with payload: {:#x?}",
+                transfer.destination,
+                &transfer.payload.0
+            )
+        }
+        _ => {}
+    }
+    Ok(())
 }
