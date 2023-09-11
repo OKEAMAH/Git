a3d0f827075f1d8247e2e879de744237478abcf0
martoon <martoon.hsk@gmail.com>
2023-09-12 02:51:16 +0300

    MIR: Add tests on new logic

--- contrib/mir/src/rollup/demo.rs
+++ contrib/mir/src/rollup/demo.rs
@@ -80,3 +80,47 @@ fn free_form_bytes_to_number(bs: &[u8]) -> usize {
     bs_sized[..bs_len].copy_from_slice(&bs[..bs_len]);
     usize::from_le_bytes(bs_sized)
 }
+
+#[cfg(test)]
+mod test_message_processing {
+    use num_bigint::BigInt;
+    use tezos_crypto_rs::hash::ContractKt1Hash;
+    use tezos_smart_rollup_encoding::{
+        public_key_hash::PublicKeyHash, smart_rollup::SmartRollupAddress,
+    };
+    use tezos_smart_rollup_mock::MockHost;
+
+    use super::*;
+
+    fn test_on_sample(param: &[u8], expected_res: &[u8]) {
+        let mut host = MockHost::default();
+        process_external_message(&mut host, param).unwrap();
+        assert_eq!(host.store_read_all(STORAGE_PATH).unwrap(), expected_res);
+    }
+
+    #[test]
+    fn test_external_message_processing_on_samples() {
+        test_on_sample(b"\x00", b"\0\0\0\0\0\0\0\0");
+        test_on_sample(b"\x01", b"\x01\0\0\0\0\0\0\0");
+        test_on_sample(&[10], &[55, 0, 0, 0, 0, 0, 0, 0]);
+        test_on_sample(&[15], &[98, 2, 0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_internal_message_processing_on_samples() {
+        let mut host = MockHost::default();
+        let payload = MichelsonInt(BigInt::from(10).into());
+        let transfer = Transfer {
+            payload,
+            sender: ContractKt1Hash::from_base58_check("KT1ThEdxfUcWUwqsdergy3QnbCWGHSUHeHJq")
+                .unwrap(),
+            source: PublicKeyHash::from_b58check("tz1RjtZUVeLhADFHDL8UwDZA6vjWWhojpu5w").unwrap(),
+            destination: SmartRollupAddress::new(host.reveal_metadata().address()),
+        };
+        process_internal_message(&mut host, &transfer).unwrap();
+        assert_eq!(
+            host.store_read_all(STORAGE_PATH).unwrap(),
+            &[55, 0, 0, 0, 0, 0, 0, 0]
+        );
+    }
+}
