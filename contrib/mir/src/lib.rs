197cfce6b051d302a6c63fbd5a1b7bfb11316d60
martoon <martoon.hsk@gmail.com>
2023-09-13 13:19:12 +0300

    MIR: Deal with unused warnings
    Problem: for all targets except `wasm32` I get warnings about all kernel
    code being unused. This happens because `kernel_entry!` macro exposes
    our kernel only when compiled for `wasm32`.
    
    Solution: add a dummy use of `kernel_entry` for non-wasm32 targets.

--- contrib/mir/src/lib.rs
+++ contrib/mir/src/lib.rs
@@ -6,6 +6,16 @@
 /******************************************************************************/
 
 mod rollup;
-use tezos_smart_rollup::kernel_entry;
+use rollup::kernel::kernel_entry;
+use tezos_smart_rollup::{kernel_entry, prelude::Runtime};
 
 kernel_entry!(rollup::kernel::kernel_entry);
+
+// kernel_entry! does something only on wasm32 target, on others we should deal
+// with `kernel_entry` being unused.
+#[cfg(not(feature = "wasm32-unknown-unknown"))]
+#[allow(dead_code)]
+fn consider_kernel_used(host: &mut impl Runtime) -> ! {
+    let _ = kernel_entry(host);
+    panic!("Should not be called");
+}
