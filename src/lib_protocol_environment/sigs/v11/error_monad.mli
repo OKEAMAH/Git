205d838972681bb77920e77f72cd37e277ea1068
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2023-10-02 14:51:23 +0200

    Env v11: remove Error_monad.return_false

--- src/lib_protocol_environment/sigs/v11/error_monad.mli
+++ src/lib_protocol_environment/sigs/v11/error_monad.mli
@@ -78,8 +78,6 @@ val pp_trace : Format.formatter -> error trace -> unit
 
 val result_encoding : 'a Data_encoding.t -> 'a tzresult Data_encoding.t
 
-val return_false : (bool, 'trace) result Lwt.t
-
 val error : 'err -> ('a, 'err trace) result
 
 val trace_of_error : 'err -> 'err trace
