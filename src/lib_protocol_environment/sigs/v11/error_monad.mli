5ab4cc7f8cdb8d7cc90dab4459fb9b3fcbeb348a
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2023-10-02 14:29:15 +0200

    Env v11: remove Error_monad.return_none

--- src/lib_protocol_environment/sigs/v11/error_monad.mli
+++ src/lib_protocol_environment/sigs/v11/error_monad.mli
@@ -78,8 +78,6 @@ val pp_trace : Format.formatter -> error trace -> unit
 
 val result_encoding : 'a Data_encoding.t -> 'a tzresult Data_encoding.t
 
-val return_none : ('a option, 'trace) result Lwt.t
-
 val return_some : 'a -> ('a option, 'trace) result Lwt.t
 
 val return_nil : ('a list, 'trace) result Lwt.t
