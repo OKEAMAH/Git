babb5aa88ad92aabbe0a2df35fe31d3b001367f6
martoon <martoon.hsk@gmail.com>
2023-10-05 12:39:31 +0400

    MIR: Add `rollup_util` umbrella module
    Various utilities like several layers of durable storage API and debug
    logging will go here.

--- contrib/mir/src/rollup_util.rs
+++ contrib/mir/src/rollup_util.rs
@@ -4,20 +4,3 @@
 /* Copyright (c) [2023] Serokell <hi@serokell.io>                             */
 /*                                                                            */
 /******************************************************************************/
-
-mod rollup;
-mod rollup_util;
-
-use rollup::kernel::kernel_entry;
-use tezos_smart_rollup::{kernel_entry, prelude::Runtime};
-
-kernel_entry!(rollup::kernel::kernel_entry);
-
-// kernel_entry! does something only on wasm32 target, on others we should deal
-// with `kernel_entry` being unused.
-#[cfg(not(feature = "wasm32-unknown-unknown"))]
-#[allow(dead_code)]
-fn consider_kernel_used(host: &mut impl Runtime) -> ! {
-    let _ = kernel_entry(host);
-    panic!("Should not be called");
-}
