0fff4256949601b5fc19482a4c728f7155c32864
martoon <martoon.hsk@gmail.com>
2023-10-05 13:31:46 +0400

    MIR: Add high-level storage operations
    Problem: the durable storage API provided by `tezos_smart_rollup` set of
    libraries is inconvenient in practice. Just to check the storage for a
    value I have to:
    * Either check for the key presence first, or sort out the errors.
    * Do something with the errors, some of which are impossible, and most
      of which are not recoverable.
    * Overall, not to forget that the key can be not present in the storage,
      because the natural wish is to just to propagate the error.
    
    Solution: start a layer on top of that API that:
    * Hides operations for working with slices for now, we only read or
      write entire value at a time.
    * Sorts out the errors: key absence is reported via `None`, all
      de-facto unchecked errors are turned into panics. Examples of such
      errors: key is too large, some generic error at the host side.
    * Very clearly returns the important information, like whether `get`
      found no specified key, or whether `move` has actually moved anything.
    
    The point about panics is arguable, we may want to consider going with
    explicit `InternalError` put in `Result`, but I don't see it being
    beneficial:
    * Those errors are mostly always a programmer mistake;
    * There is no way to recover from them;
    * They are trully internal, we never want to report them to the user.
    
    This commits yet only adds two functions necessary for us - reading and
    writing.

---
+++ contrib/mir/src/rollup_util/storage.rs
@@ -0,0 +1,118 @@
+/******************************************************************************/
+/*                                                                            */
+/* SPDX-License-Identifier: MIT                                               */
+/* Copyright (c) [2023] Serokell <hi@serokell.io>                             */
+/*                                                                            */
+/******************************************************************************/
+
+//! Provides higher-level host operations, this is a layer on top of [Runtime].
+//!
+//! Things that are related to all the methods:
+//!
+//! Mind the size limits: values cannot be larger than 2Gb, and keys also have
+//! their limit on size. Consider avoiding using arbitrarily-sized path
+//! seghments in the keys. And we will have to do something about values.
+//!
+//! There are a few restrictions on keys. Putting it short, the key must use
+//! characters from `[A-Za-z0-9.-_]` set, and there are busy keys used by
+//! rollup machinery (just avoid `/readonly` and `/kernel` prefixes). For more
+//! details, see [tezos_smart_rollup_host::path::PathError].
+
+use tezos_smart_rollup::{host::RuntimeError, prelude::Runtime, storage::path::Path};
+
+// A brief introduction to the durable storage:
+//
+// Durable storage is a key-value store; however it is also similar to a
+// filesystem. So keys look like paths, and you have values (aka files) and
+// subtrees (aka directories).
+//
+// *Unlike* in filesystem:
+// * There is no need to create directories manually.
+// * Under a key we can have a directory and a value simultanously.
+//
+// So you don't need to care about directories until you have a case for batch
+// delete of values or for counting the number of `/prefix/XXX` keys in the
+// storage.
+//
+// If behaviour of any function is unclear, see the tests at the bottom of this
+// module. If your case is not covered, feel free to add it!
+
+/// Turn a [RuntimeError] into panic.
+///
+/// This must be used only when you have already checked the provided error for
+/// all checked errors (i.e. those that are entirely expected in certain
+/// circumstances).
+///
+/// In the majority of the cases getting [HostError] indicates either a
+/// programmer mistake (e.g. invalid key constructed, value is oversized), or
+/// that something went utterly wrong (we probably can consider
+/// [HostError::GenericInvalidAccess] to be from this category). There is no
+/// sensible way to recover from these errors, so need to explicitly propagate
+/// them.
+///
+/// Still use this method with care; e.g. [HostError::StoreNotAValue] in theory
+/// can indicate a normal condition.
+macro_rules! panic_with_runtime_err {
+    ( $err:expr ) => {
+        panic!("Unexpected error when working with storage: {}", $err)
+    };
+}
+
+fn is_key_absent_error(err: RuntimeError) -> bool {
+    matches!(
+        err,
+        RuntimeError::PathNotFound
+        // PathNotFound is not reliable - it is returned when the inner
+        // `store_has` says there is something there, i.e. when either value or
+        // subtree or both are there.
+        //
+        // Oftentimes this is not what we want: for instance, in `store_get` we
+        // access only a value. So on running `store_get`, if there is nethier
+        // value nor subtree at this key, I will get PathNotFound. If there is
+        // only a subtree, I will get `StoreNotAValue`.
+        | RuntimeError::HostErr(
+            tezos_smart_rollup_host::Error::StoreNotANode
+            | tezos_smart_rollup_host::Error::StoreNotAValue)
+    )
+}
+
+/// NB on implementation (@martoon) [optimization]: This API is implemented
+/// on top of tezos_smart_rollup's [Runtime], but my impression is that it
+/// does not really suit our purposes well:
+/// 1. Instead of being more elaborate on which errors can be throw and
+///    which cannot, it just rethrows all and adds its own options.
+///
+///    It does not attempt at distinguishing checked and unchecked errors.
+///
+///    Moreover, it adds ambiguity: `HostError` has its own errors for "key
+///    not found", and `RuntimeError` additionally declares a dedicated
+///    constructor for that (that does not really work well as explained in
+///    [is_key_absent_error]).
+///
+/// 2. In case of reading methods, the implementation always checks for the
+///    key presence first (instead of relying on error codes). This sounds
+///    like an extra work with memory (however its amount can be nigligible
+///    due to cache, needs benchmarking).
+
+/// Read a value from the durable storage,
+///
+/// This is a high-level function. It is dedicated for reading entire,
+/// possibly large, values, but does not allow reading only a slice of data.
+pub fn read_all(host: &impl Runtime, path: &impl Path) -> Option<Vec<u8>> {
+    host.store_read_all(path)
+        .map_err(|err| {
+            if !is_key_absent_error(err) {
+                panic_with_runtime_err!(err)
+            }
+        })
+        .ok()
+}
+
+/// Put a value to the durable storage.
+///
+/// This is a high-level function. It is dedicated for writing entire,
+/// possibly large, values, but does not allow writing only a slice of data.
+pub fn write_all(host: &mut impl Runtime, path: &impl Path, content: &[u8]) {
+    host.store_write_all(path, content)
+        .unwrap_or_else(|err| panic_with_runtime_err!(err))
+}
