313999806ddeb0d3ff9159287fd5d3ea8d0174e1
Hantang Sun <hantang.sun@trili.tech>
2023-10-09 17:04:10 +0100

    EVM: fix arithmetic underflow

--- src/kernel_evm/kernel/src/apply.rs
+++ src/kernel_evm/kernel/src/apply.rs
@@ -52,7 +52,9 @@ impl Transaction {
             TransactionContent::Ethereum(transaction) => {
                 let priority_fee_per_gas = U256::min(
                     transaction.max_priority_fee_per_gas,
-                    transaction.max_fee_per_gas - block_base_fee_per_gas,
+                    transaction
+                        .max_fee_per_gas
+                        .saturating_sub(block_base_fee_per_gas),
                 );
                 priority_fee_per_gas + block_base_fee_per_gas
             }
