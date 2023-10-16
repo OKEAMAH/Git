8e84f9630a9049f2a0fe934fdcc6f5ee483e7b18
Gauthier SEBILLE <gauthier@marigold.dev>
2023-10-16 12:16:43 +0200

    RBT: generic reset_block_section for baking_profiler
    RBT: drop hashtabl for performance issue

--- src/proto_alpha/lib_delegate/baking_profiler.ml
+++ src/proto_alpha/lib_delegate/baking_profiler.ml
@@ -16,4 +16,20 @@ let init profiler_maker =
   plug node_rpc_profiler (profiler_maker ~name:"node_rpc") ;
   plug operation_worker_profiler (profiler_maker ~name:"op_worker")
 
+let create_reset_block_section profiler =
+  let last_block = ref None in
+  fun b ->
+    let sec () = Format.asprintf "%a" Block_hash.pp b in
+    match !last_block with
+    | None ->
+        let s = sec () in
+        record profiler s ;
+        last_block := Some b
+    | Some b' when Block_hash.equal b' b -> ()
+    | Some _ ->
+        stop profiler ;
+        let s = sec () in
+        record profiler s ;
+        last_block := Some b
+
 include (val wrap profiler)
