a1c884884cc2cd9d8b3ad10413e99b24ae954d08
Hugo Heuzard <hugo.heuzard@nomadic-labs.com>
2023-11-06 10:41:51 +0100

    TEMP: vendor ocaml-yaml

---
+++ vendors/ocaml-yaml/tests/test_parse.ml
@@ -0,0 +1,11 @@
+let ( >>= ) = Result.bind
+
+let v file =
+  Bos.OS.File.read file >>= fun buf ->
+  Yaml.yaml_of_string buf >>= fun v ->
+  Yaml.to_json v >>= fun json ->
+  Ezjsonm.to_string (Ezjsonm.wrap json) |> fun b ->
+  Printf.printf "%s\n%!" b;
+  Yaml.yaml_to_string v >>= fun s ->
+  Printf.printf "%s\n%!" s;
+  Ok ()
