5fa23b68d9c7f60cc0a3498da2aecca15bb96c86
Alain Mebsout <alain.mebsout@functori.com>
2023-11-08 14:39:01 +0100

    Test: snapshot exports

--- tezt/lib_tezos/sc_rollup_node.ml
+++ tezt/lib_tezos/sc_rollup_node.ml
@@ -606,6 +606,20 @@ let dump_durable_storage ~sc_rollup_node ~dump ?(block = "head") () =
   let process = spawn_command sc_rollup_node cmd in
   Process.check process
 
+let export_snapshot sc_rollup_node dir =
+  let process =
+    spawn_command
+      sc_rollup_node
+      ["export"; "snapshot"; "into"; dir; "--data-dir"; data_dir sc_rollup_node]
+  in
+  let parse process =
+    let* output = Process.check_and_read_stdout process in
+    match output =~* rex "Snapshot exported to ([^\n]*)" with
+    | None -> failwith "Snapshot export failed"
+    | Some filename -> return filename
+  in
+  Runnable.{value = process; run = parse}
+
 let as_rpc_endpoint (t : t) =
   let state = t.persistent_state in
   let scheme = "http" in
