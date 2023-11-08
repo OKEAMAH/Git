8b9112192cd4c71ea8a3a0dbd92aa19bd3a07e84
marcbeunardeau <marc.beunardeau@nomadic-labs.com>
2023-11-08 18:07:04 +0100

    temp update

--- src/lib_kzg/verkle.ml
+++ src/lib_kzg/verkle.ml
@@ -24,11 +24,27 @@ let () = assert (log_size mod 2 = 0)
 
 let trap_door = Scalar.random ()
 
-let srs = Srs_g1.generate_insecure (1 lsl log_size) trap_door
+let srs = Srs_g1.generate_insecure array_size trap_door
+
+let domain = Domain.build array_size
+
+let srs_c_array = G1_carray.init array_size (fun i -> Srs_g1.get srs i)
+
+(*TODO : check*)
+let srs_lagrange =
+  let () = G1_carray.interpolation_ecfft_inplace ~domain ~points:srs_c_array in
+  srs_c_array
 
 let hash_ec_to_fr ec =
   let hash = Hacl_star.Hacl.Blake2b_32.hash (G1.to_bytes ec) 32 in
   Z.of_bits (Bytes.to_string hash) |> Scalar.of_z
+
+let formula_snd fst snd =
+  G1.size_in_bytes + fst_lvl_len
+  + (fst * array_size * Scalar.size_in_bytes)
+  + (snd * Scalar.size_in_bytes)
+
+let formula_fst_level i = G1.size_in_bytes + (Scalar.size_in_bytes * i)
 
 let read_file file_descr buffer ~offset ~len =
   assert (Bytes.length buffer = len) ;
@@ -57,7 +73,6 @@ let create_storage ?(test = false) file_name =
         if test then Scalar.of_int i else Scalar.random ())
   in
   let nb_vectors = 1 lsl (log_size / 2) in
-  let domain = Domain.build (1 lsl (log_size / 2)) in
   let snd_lvl = Array.init nb_vectors (fun _ -> random_vector ()) in
   let fst_lvl =
     Array.map
@@ -159,7 +174,72 @@ let create_diff size =
   in
   repeat f init size
 
-(* let update_storage file_name diff =
- *   let file_descr = Unix.openfile file_name [O_CREAT; O_RDWR] 0o640 in
- *   () *)
-(*update sndlvl *)
+let map_to_array map =
+  List.map snd (List.of_seq (IntMap.to_seq map)) |> Array.of_list
+
+let update_storage file_name diff =
+  let file_descr = Unix.openfile file_name [O_CREAT; O_RDWR] 0o640 in
+  let update_i_fst_lvl snd_lvl_diff =
+    let srs_lagrange_list = G1_carray.to_array srs_lagrange |> Array.to_list in
+    let filtered_list =
+      List.filteri (fun i _ -> IntMap.mem i snd_lvl_diff) srs_lagrange_list
+    in
+    let to_pippinger_ec = filtered_list |> Array.of_list in
+    let to_pippinger_fr = map_to_array snd_lvl_diff in
+    G1.pippenger to_pippinger_ec to_pippinger_fr
+  in
+  let fst_lvl_diff_ec = IntMap.map update_i_fst_lvl diff in
+
+  let to_pippinger_fr_root =
+    IntMap.mapi
+      (fun i _ ->
+        let buffer = Bytes.create Scalar.size_in_bytes in
+        let () =
+          read_file
+            file_descr
+            buffer
+            ~offset:(formula_fst_level i)
+            ~len:Scalar.size_in_bytes
+        in
+        let old_hash = buffer |> Scalar.of_bytes_exn in
+        let new_hash = IntMap.find i fst_lvl_diff_ec |> hash_ec_to_fr in
+        Scalar.(sub new_hash old_hash))
+      diff
+  in
+
+  let srs_lagrange_list = G1_carray.to_array srs_lagrange |> Array.to_list in
+  let filtered_list =
+    List.filteri (fun i _ -> IntMap.mem i diff) srs_lagrange_list
+  in
+  let to_pippinger_ec_root = filtered_list |> Array.of_list in
+  let update_root =
+    G1.pippenger to_pippinger_ec_root (map_to_array to_pippinger_fr_root)
+  in
+
+  (*update sndlvl *)
+  let fr_buffer = Bytes.create Scalar.size_in_bytes in
+  let snd_lvl_map fst snd_lvl_diff =
+    IntMap.mapi
+      (fun snd diff ->
+        let () =
+          read_file
+            file_descr
+            fr_buffer
+            ~offset:(formula_snd fst snd)
+            ~len:Scalar.size_in_bytes
+        in
+        Scalar.(of_bytes_exn fr_buffer + diff))
+      snd_lvl_diff
+  in
+  let to_write = IntMap.mapi snd_lvl_map diff in
+  let snd_lvl_iter fst snd_lvl_to_write =
+    IntMap.iter
+      (fun snd to_write ->
+        write_file
+          file_descr
+          (Scalar.to_bytes to_write)
+          ~offset:(formula_snd fst snd)
+          ~len:Scalar.size_in_bytes)
+      snd_lvl_to_write
+  in
+  IntMap.iter snd_lvl_iter to_write
