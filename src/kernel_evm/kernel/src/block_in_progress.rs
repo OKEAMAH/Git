mavryk-network/mavryk-protocol

EVM/Kernel: index logs in transaction receipt

src/kernel_evm/kernel/src/block_in_progress.rs
@@ -20,8 +20,8 @@ use std::collections::VecDeque;
 use tezos_ethereum::block::{BlockConstants, L2Block};
 use tezos_ethereum::rlp_helpers::*;
 use tezos_ethereum::transaction::{
-    TransactionObject, TransactionReceipt, TransactionStatus, TransactionType,
-    TRANSACTION_HASH_SIZE,
+    IndexedLog, TransactionObject, TransactionReceipt, TransactionStatus,
+    TransactionType, TRANSACTION_HASH_SIZE,
 };
 use tezos_ethereum::Bloom;
 use tezos_smart_rollup_host::path::RefPath;
@@ -46,12 +46,15 @@ pub struct BlockInProgress {
     pub parent_hash: H256,
     /// Cumulative number of ticks used
     pub estimated_ticks: u64,
+    /// logs bloom filter
     pub logs_bloom: Bloom,
+    /// offset for the first log of the next transaction
+    pub logs_offset: u64,
 }
 
 impl Encodable for BlockInProgress {
     fn rlp_append(&self, stream: &mut rlp::RlpStream) {
-        stream.begin_list(8);
+        stream.begin_list(9);
         stream.append(&self.number);
         append_queue(stream, &self.tx_queue);
         append_txs(stream, &self.valid_txs);
@@ -60,6 +63,7 @@ impl Encodable for BlockInProgress {
         stream.append(&self.gas_price);
         stream.append(&self.parent_hash);
         stream.append(&self.logs_bloom);
+        stream.append(&self.logs_offset);
     }
 }
 
@@ -82,7 +86,7 @@ impl Decodable for BlockInProgress {
         if !decoder.is_list() {
             return Err(DecoderError::RlpExpectedToBeList);
         }
-        if decoder.item_count()? != 8 {
+        if decoder.item_count()? != 9 {
             return Err(DecoderError::RlpIncorrectListLen);
         }
 
@@ -96,6 +100,7 @@ impl Decodable for BlockInProgress {
         let gas_price: U256 = decode_field(&next(&mut it)?, "gas_price")?;
         let parent_hash: H256 = decode_field(&next(&mut it)?, "parent_hash")?;
         let logs_bloom: Bloom = decode_field(&next(&mut it)?, "logs_bloom")?;
+        let logs_offset: u64 = decode_field(&next(&mut it)?, "logs_offset")?;
         let estimated_ticks: u64 = 0;
         let bip = Self {
             number,
@@ -107,6 +112,7 @@ impl Decodable for BlockInProgress {
             parent_hash,
             estimated_ticks,
             logs_bloom,
+            logs_offset,
         };
         Ok(bip)
     }
@@ -160,6 +166,7 @@ impl BlockInProgress {
             parent_hash,
             estimated_ticks,
             logs_bloom: Bloom::default(),
+            logs_offset: 0,
         }
     }
 
@@ -295,7 +302,7 @@ impl BlockInProgress {
     }
 
     pub fn make_receipt(
-        &self,
+        &mut self,
         receipt_info: TransactionReceiptInfo,
     ) -> TransactionReceipt {
         let TransactionReceiptInfo {
@@ -307,33 +314,47 @@ impl BlockInProgress {
             ..
         } = receipt_info;
 
-        let &Self {
+        let &mut Self {
             number: block_number,
             gas_price: effective_gas_price,
             cumulative_gas,
+            logs_offset,
             ..
         } = self;
 
         match execution_outcome {
-            Some(outcome) => TransactionReceipt {
-                hash,
-                index,
-                block_number,
-                from,
-                to,
-                cumulative_gas_used: cumulative_gas,
-                effective_gas_price,
-                gas_used: U256::from(outcome.gas_used),
-                contract_address: outcome.new_address,
-                logs_bloom: TransactionReceipt::logs_to_bloom(&outcome.logs),
-                logs: outcome.logs,
-                type_: TransactionType::Legacy,
-                status: if outcome.is_success {
-                    TransactionStatus::Success
-                } else {
-                    TransactionStatus::Failure
-                },
-            },
+            Some(outcome) => {
+                // index logs
+                let logs: Vec<IndexedLog> = outcome
+                    .logs
+                    .iter()
+                    .enumerate()
+                    .map(|(i, log)| IndexedLog {
+                        log: log.clone(),
+                        index: i as u64 + logs_offset,
+                    })
+                    .collect();
+                self.logs_offset += logs.len() as u64;
+                TransactionReceipt {
+                    hash,
+                    index,
+                    block_number,
+                    from,
+                    to,
+                    cumulative_gas_used: cumulative_gas,
+                    effective_gas_price,
+                    gas_used: U256::from(outcome.gas_used),
+                    contract_address: outcome.new_address,
+                    logs_bloom: TransactionReceipt::logs_to_bloom(&logs),
+                    logs,
+                    type_: TransactionType::Legacy,
+                    status: if outcome.is_success {
+                        TransactionStatus::Success
+                    } else {
+                        TransactionStatus::Failure
+                    },
+                }
+            }
             None => TransactionReceipt {
                 hash,
                 index,
@@ -438,10 +459,11 @@ mod tests {
             parent_hash: H256::from([5; 32]),
             estimated_ticks: 99,
             logs_bloom: Bloom::default(),
+            logs_offset: 33,
         };
 
         let encoded = bip.rlp_bytes();
-        let expected = "f902542af8e6f871a00101010101010101010101010101010101010101010101010101010101010101f84e01b84bf84901010180018026a00101010101010101010101010101010101010101010101010101010101010101a00101010101010101010101010101010101010101010101010101010101010101f871a00808080808080808080808080808080808080808080808080808080808080808f84e01b84bf84908080880088034a00808080808080808080808080808080808080808080808080808080808080808a00808080808080808080808080808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        let expected = "f902552af8e6f871a00101010101010101010101010101010101010101010101010101010101010101f84e01b84bf84901010180018026a00101010101010101010101010101010101010101010101010101010101010101a00101010101010101010101010101010101010101010101010101010101010101f871a00808080808080808080808080808080808080808080808080808080808080808f84e01b84bf84908080880088034a00808080808080808080808080808080808080808080808080808080808080808a00808080808080808080808080808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000021";
         assert_eq!(hex::encode(encoded), expected);
 
         let bytes = hex::decode(expected).expect("Should be valid hex string");
@@ -469,10 +491,11 @@ mod tests {
             parent_hash: H256::from([5; 32]),
             estimated_ticks: 99,
             logs_bloom: Bloom::default(),
+            logs_offset: 0,
         };
 
         let encoded = bip.rlp_bytes();
-        let expected = "f901e82af87af83ba00101010101010101010101010101010101010101010101010101010101010101d902d70101940101010101010101010101010101010101010101f83ba00808080808080808080808080808080808080808080808080808080808080808d902d70808940808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        let expected = "f901e92af87af83ba00101010101010101010101010101010101010101010101010101010101010101d902d70101940101010101010101010101010101010101010101f83ba00808080808080808080808080808080808080808080808080808080808080808d902d70808940808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000080";
         assert_eq!(hex::encode(encoded), expected);
 
         let bytes = hex::decode(expected).expect("Should be valid hex string");
@@ -500,10 +523,11 @@ mod tests {
             parent_hash: H256::from([5; 32]),
             estimated_ticks: 99,
             logs_bloom: Bloom::default(),
+            logs_offset: 4,
         };
 
         let encoded = bip.rlp_bytes();
-        let expected = "f9021e2af8b0f871a00101010101010101010101010101010101010101010101010101010101010101f84e01b84bf84901010180018026a00101010101010101010101010101010101010101010101010101010101010101a00101010101010101010101010101010101010101010101010101010101010101f83ba00808080808080808080808080808080808080808080808080808080808080808d902d70808940808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        let expected = "f9021f2af8b0f871a00101010101010101010101010101010101010101010101010101010101010101f84e01b84bf84901010180018026a00101010101010101010101010101010101010101010101010101010101010101a00101010101010101010101010101010101010101010101010101010101010101f83ba00808080808080808080808080808080808080808080808080808080808080808d902d70808940808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004";
         assert_eq!(hex::encode(encoded), expected);
 
         let bytes = hex::decode(expected).expect("Should be valid hex string");

OKEAMAH/Git

src/kernel_evm/kernel/src/block_in_progress.rs
@@ -21,8 +21,8 @@ use std::collections::VecDeque;
 use tezos_ethereum::block::{BlockConstants, L2Block};
 use tezos_ethereum::rlp_helpers::*;
 use tezos_ethereum::transaction::{
-    TransactionObject, TransactionReceipt, TransactionStatus, TransactionType,
-    TRANSACTION_HASH_SIZE,
+    IndexedLog, TransactionObject, TransactionReceipt, TransactionStatus,
+    TransactionType, TRANSACTION_HASH_SIZE,
 };
 use tezos_ethereum::Bloom;
 use tezos_smart_rollup_host::path::RefPath;
@@ -47,12 +47,15 @@ pub struct BlockInProgress {
     pub parent_hash: H256,
     /// Cumulative number of ticks used
     pub estimated_ticks: u64,
+    /// logs bloom filter
     pub logs_bloom: Bloom,
+    /// offset for the first log of the next transaction
+    pub logs_offset: u64,
 }
 
 impl Encodable for BlockInProgress {
     fn rlp_append(&self, stream: &mut rlp::RlpStream) {
-        stream.begin_list(8);
+        stream.begin_list(9);
         stream.append(&self.number);
         append_queue(stream, &self.tx_queue);
         append_txs(stream, &self.valid_txs);
@@ -61,6 +64,7 @@ impl Encodable for BlockInProgress {
         stream.append(&self.gas_price);
         stream.append(&self.parent_hash);
         stream.append(&self.logs_bloom);
+        stream.append(&self.logs_offset);
     }
 }
 
@@ -83,7 +87,7 @@ impl Decodable for BlockInProgress {
         if !decoder.is_list() {
             return Err(DecoderError::RlpExpectedToBeList);
         }
-        if decoder.item_count()? != 8 {
+        if decoder.item_count()? != 9 {
             return Err(DecoderError::RlpIncorrectListLen);
         }
 
@@ -97,6 +101,7 @@ impl Decodable for BlockInProgress {
         let gas_price: U256 = decode_field(&next(&mut it)?, "gas_price")?;
         let parent_hash: H256 = decode_field(&next(&mut it)?, "parent_hash")?;
         let logs_bloom: Bloom = decode_field(&next(&mut it)?, "logs_bloom")?;
+        let logs_offset: u64 = decode_field(&next(&mut it)?, "logs_offset")?;
         let estimated_ticks: u64 = 0;
         let bip = Self {
             number,
@@ -108,6 +113,7 @@ impl Decodable for BlockInProgress {
             parent_hash,
             estimated_ticks,
             logs_bloom,
+            logs_offset,
         };
         Ok(bip)
     }
@@ -161,6 +167,7 @@ impl BlockInProgress {
             parent_hash,
             estimated_ticks,
             logs_bloom: Bloom::default(),
+            logs_offset: 0,
         }
     }
 
@@ -308,7 +315,7 @@ impl BlockInProgress {
     }
 
     pub fn make_receipt(
-        &self,
+        &mut self,
         receipt_info: TransactionReceiptInfo,
     ) -> TransactionReceipt {
         let TransactionReceiptInfo {
@@ -320,33 +327,45 @@ impl BlockInProgress {
             ..
         } = receipt_info;
 
-        let &Self {
+        let &mut Self {
             number: block_number,
             gas_price: effective_gas_price,
             cumulative_gas,
+            logs_offset,
             ..
         } = self;
 
         match execution_outcome {
-            Some(outcome) => TransactionReceipt {
-                hash,
-                index,
-                block_number,
-                from,
-                to,
-                cumulative_gas_used: cumulative_gas,
-                effective_gas_price,
-                gas_used: U256::from(outcome.gas_used),
-                contract_address: outcome.new_address,
-                logs_bloom: TransactionReceipt::logs_to_bloom(&outcome.logs),
-                logs: outcome.logs,
-                type_: TransactionType::Legacy,
-                status: if outcome.is_success {
-                    TransactionStatus::Success
-                } else {
-                    TransactionStatus::Failure
-                },
-            },
+            Some(outcome) => {
+                let log_iter = outcome.logs.into_iter();
+                let logs: Vec<IndexedLog> = log_iter
+                    .enumerate()
+                    .map(|(i, log)| IndexedLog {
+                        log,
+                        index: i as u64 + logs_offset,
+                    })
+                    .collect();
+                self.logs_offset += logs.len() as u64;
+                TransactionReceipt {
+                    hash,
+                    index,
+                    block_number,
+                    from,
+                    to,
+                    cumulative_gas_used: cumulative_gas,
+                    effective_gas_price,
+                    gas_used: U256::from(outcome.gas_used),
+                    contract_address: outcome.new_address,
+                    logs_bloom: TransactionReceipt::logs_to_bloom(&logs),
+                    logs,
+                    type_: TransactionType::Legacy,
+                    status: if outcome.is_success {
+                        TransactionStatus::Success
+                    } else {
+                        TransactionStatus::Failure
+                    },
+                }
+            }
             None => TransactionReceipt {
                 hash,
                 index,
@@ -451,10 +470,11 @@ mod tests {
             parent_hash: H256::from([5; 32]),
             estimated_ticks: 99,
             logs_bloom: Bloom::default(),
+            logs_offset: 33,
         };
 
         let encoded = bip.rlp_bytes();
-        let expected = "f902542af8e6f871a00101010101010101010101010101010101010101010101010101010101010101f84e01b84bf84901010180018026a00101010101010101010101010101010101010101010101010101010101010101a00101010101010101010101010101010101010101010101010101010101010101f871a00808080808080808080808080808080808080808080808080808080808080808f84e01b84bf84908080880088034a00808080808080808080808080808080808080808080808080808080808080808a00808080808080808080808080808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        let expected = "f902552af8e6f871a00101010101010101010101010101010101010101010101010101010101010101f84e01b84bf84901010180018026a00101010101010101010101010101010101010101010101010101010101010101a00101010101010101010101010101010101010101010101010101010101010101f871a00808080808080808080808080808080808080808080808080808080808080808f84e01b84bf84908080880088034a00808080808080808080808080808080808080808080808080808080808080808a00808080808080808080808080808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000021";
         assert_eq!(hex::encode(encoded), expected);
 
         let bytes = hex::decode(expected).expect("Should be valid hex string");
@@ -482,10 +502,11 @@ mod tests {
             parent_hash: H256::from([5; 32]),
             estimated_ticks: 99,
             logs_bloom: Bloom::default(),
+            logs_offset: 0,
         };
 
         let encoded = bip.rlp_bytes();
-        let expected = "f901e82af87af83ba00101010101010101010101010101010101010101010101010101010101010101d902d70101940101010101010101010101010101010101010101f83ba00808080808080808080808080808080808080808080808080808080808080808d902d70808940808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        let expected = "f901e92af87af83ba00101010101010101010101010101010101010101010101010101010101010101d902d70101940101010101010101010101010101010101010101f83ba00808080808080808080808080808080808080808080808080808080808080808d902d70808940808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000080";
         assert_eq!(hex::encode(encoded), expected);
 
         let bytes = hex::decode(expected).expect("Should be valid hex string");
@@ -513,10 +534,11 @@ mod tests {
             parent_hash: H256::from([5; 32]),
             estimated_ticks: 99,
             logs_bloom: Bloom::default(),
+            logs_offset: 4,
         };
 
         let encoded = bip.rlp_bytes();
-        let expected = "f9021e2af8b0f871a00101010101010101010101010101010101010101010101010101010101010101f84e01b84bf84901010180018026a00101010101010101010101010101010101010101010101010101010101010101a00101010101010101010101010101010101010101010101010101010101010101f83ba00808080808080808080808080808080808080808080808080808080808080808d902d70808940808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        let expected = "f9021f2af8b0f871a00101010101010101010101010101010101010101010101010101010101010101f84e01b84bf84901010180018026a00101010101010101010101010101010101010101010101010101010101010101a00101010101010101010101010101010101010101010101010101010101010101f83ba00808080808080808080808080808080808080808080808080808080808080808d902d70808940808080808080808080808080808080808080808f842a00202020202020202020202020202020202020202020202020202020202020202a00909090909090909090909090909090909090909090909090909090909090909030405a00505050505050505050505050505050505050505050505050505050505050505b901000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004";
         assert_eq!(hex::encode(encoded), expected);
 
         let bytes = hex::decode(expected).expect("Should be valid hex string");