mavryk-network/mavryk-protocol

rollup/node: add gadt for purpose and operator

src/lib_smart_rollup_node/node_context.ml
@@ -74,12 +74,10 @@ type rw = [`Read | `Write] t
 type ro = [`Read] t
 
 let get_operator node_ctxt purpose =
-  Purpose.Map.find purpose node_ctxt.config.operators
+  Purpose.find_operator purpose node_ctxt.config.operators
 
 let is_operator node_ctxt pkh =
-  Purpose.Map.exists
-    (fun _ operator -> Signature.Public_key_hash.(operator = pkh))
-    node_ctxt.config.operators
+  Purpose.mem_operator pkh node_ctxt.config.operators
 
 let is_accuser node_ctxt = node_ctxt.config.mode = Accuser
 
@@ -91,9 +89,9 @@ let can_inject node_ctxt (op_kind : Operation_kind.t) =
   Configuration.can_inject node_ctxt.config.mode op_kind
 
 let check_op_in_whitelist_or_bailout_mode (node_ctxt : _ t) whitelist =
-  let operator = get_operator node_ctxt Purpose.Operating in
+  let operator = get_operator node_ctxt Operating in
   match operator with
-  | Some operator ->
+  | Some (Single operator) ->
       error_unless
         (is_bailout node_ctxt
         || List.mem ~equal:Signature.Public_key_hash.equal operator whitelist)
@@ -1147,7 +1145,12 @@ module Internal_for_tests = struct
       kind =
     let open Lwt_result_syntax in
     let rollup_address = Address.zero in
-    let operators = Purpose.Map.empty in
+    let mode = Configuration.Observer in
+    let*? operators =
+      Purpose.make_operator
+        ~needed_purpose:(Configuration.purposes_of_mode mode)
+        []
+    in
     let loser_mode = Loser_mode.no_failures in
     let l1_blocks_cache_size = Configuration.default_l1_blocks_cache_size in
     let l2_blocks_cache_size = Configuration.default_l2_blocks_cache_size in
@@ -1164,7 +1167,7 @@ module Internal_for_tests = struct
           metrics_addr = None;
           reconnection_delay = 5.;
           fee_parameters = Configuration.default_fee_parameters;
-          mode = Observer;
+          mode;
           loser_mode;
           dal_node_endpoint = None;
           dac_observer_endpoint = None;

src/lib_smart_rollup_node/node_context.mli
@@ -109,7 +109,7 @@ type ro = [`Read] t
 (** [get_operator cctxt purpose] returns the public key hash for the operator
     who has purpose [purpose], if any.
 *)
-val get_operator : _ t -> Purpose.t -> Signature.Public_key_hash.t option
+val get_operator : _ t -> 'a Purpose.purpose_kind -> 'a Purpose.operator option
 
 (** [is_operator cctxt pkh] returns [true] if the public key hash [pkh] is an
     operator for the node (for any purpose). *)

src/lib_smart_rollup_node/purpose.mli
@@ -8,14 +8,29 @@
 
 (** Purposes for operators, indicating their role and thus the kinds of
     operations that they sign. *)
-type t = Operating | Batching | Cementing | Recovering | Executing_outbox
+type 'a purpose_kind =
+  | Operating : Signature.public_key_hash purpose_kind
+  | Batching : Signature.public_key_hash purpose_kind
+  | Cementing : Signature.public_key_hash purpose_kind
+  | Recovering : Signature.public_key_hash purpose_kind
+  | Executing_outbox : Signature.public_key_hash purpose_kind
+
+type t = Purpose : 'a purpose_kind -> t
 
 (** List of possible purposes for operator specialization. *)
 val all : t list
 
 module Map : Map.S with type key = t
 
-type operators = Signature.Public_key_hash.t Map.t
+type 'a operator =
+  | Single : Signature.public_key_hash -> Signature.public_key_hash operator
+  | Multiple :
+      Signature.public_key_hash list
+      -> Signature.public_key_hash list operator
+
+type ex_operator = Operator : 'a operator -> ex_operator
+
+type operators = private ex_operator Map.t
 
 type error +=
   | Missing_operator of t
@@ -66,3 +81,18 @@ val operation_kind : t -> Operation_kind.t list
 (** For a list of operation kind, it returns a list of associated
     purpose. *)
 val of_operation_kind : Operation_kind.t list -> t list
+
+(** [find_operator purpose operators] returns the {operator} for the
+    [purpose] from the [operator]. The {`kind} type is to know if this
+    purpose allows only one or multiple operators. *)
+val find_operator : 'kind purpose_kind -> operators -> 'kind operator option
+
+(** [mem_operator operator operators] checks of [operator] is part of
+    any purpose in [operators]. *)
+val mem_operator : Signature.public_key_hash -> operators -> bool
+
+(** function to bypass the private type. The opposite is not exposed
+    so we don't risk corrupting the map with invalid ['kind1
+    purpose_kind -> 'kind2 operator] mapping where ['kind1 <>
+    'kind2] *)
+val operators_to_map : operators -> ex_operator Map.t

OKEAMAH/Git

src/lib_smart_rollup_node/node_context.ml
@@ -74,12 +74,10 @@ type rw = [`Read | `Write] t
 type ro = [`Read] t
 
 let get_operator node_ctxt purpose =
-  Purpose.Map.find purpose node_ctxt.config.operators
+  Purpose.find_operator purpose node_ctxt.config.operators
 
 let is_operator node_ctxt pkh =
-  Purpose.Map.exists
-    (fun _ operator -> Signature.Public_key_hash.(operator = pkh))
-    node_ctxt.config.operators
+  Purpose.mem_operator pkh node_ctxt.config.operators
 
 let is_accuser node_ctxt = node_ctxt.config.mode = Accuser
 
@@ -91,9 +89,9 @@ let can_inject node_ctxt (op_kind : Operation_kind.t) =
   Configuration.can_inject node_ctxt.config.mode op_kind
 
 let check_op_in_whitelist_or_bailout_mode (node_ctxt : _ t) whitelist =
-  let operator = get_operator node_ctxt Purpose.Operating in
+  let operator = get_operator node_ctxt Operating in
   match operator with
-  | Some operator ->
+  | Some (Single operator) ->
       error_unless
         (is_bailout node_ctxt
         || List.mem ~equal:Signature.Public_key_hash.equal operator whitelist)
@@ -1154,7 +1152,12 @@ module Internal_for_tests = struct
       kind =
     let open Lwt_result_syntax in
     let rollup_address = Address.zero in
-    let operators = Purpose.Map.empty in
+    let mode = Configuration.Observer in
+    let*? operators =
+      Purpose.make_operator
+        ~needed_purposes:(Configuration.purposes_of_mode mode)
+        []
+    in
     let loser_mode = Loser_mode.no_failures in
     let l1_blocks_cache_size = Configuration.default_l1_blocks_cache_size in
     let l2_blocks_cache_size = Configuration.default_l2_blocks_cache_size in
@@ -1171,7 +1174,7 @@ module Internal_for_tests = struct
           metrics_addr = None;
           reconnection_delay = 5.;
           fee_parameters = Configuration.default_fee_parameters;
-          mode = Observer;
+          mode;
           loser_mode;
           dal_node_endpoint = None;
           dac_observer_endpoint = None;

src/lib_smart_rollup_node/node_context.mli
@@ -109,7 +109,7 @@ type ro = [`Read] t
 (** [get_operator cctxt purpose] returns the public key hash for the operator
     who has purpose [purpose], if any.
 *)
-val get_operator : _ t -> Purpose.t -> Signature.Public_key_hash.t option
+val get_operator : _ t -> 'a Purpose.t -> 'a Purpose.operator option
 
 (** [is_operator cctxt pkh] returns [true] if the public key hash [pkh] is an
     operator for the node (for any purpose). *)

src/lib_smart_rollup_node/purpose.mli
@@ -8,30 +8,52 @@
 
 (** Purposes for operators, indicating their role and thus the kinds of
     operations that they sign. *)
-type t = Operating | Batching | Cementing | Recovering | Executing_outbox
+type 'a t =
+  | Operating : Signature.public_key_hash t
+  | Batching : Signature.public_key_hash t
+  | Cementing : Signature.public_key_hash t
+  | Recovering : Signature.public_key_hash t
+  | Executing_outbox : Signature.public_key_hash t
+
+(** existential type for purpose. *)
+type ex_purpose = Purpose : 'a t -> ex_purpose
 
 (** List of possible purposes for operator specialization. *)
-val all : t list
+val all : ex_purpose list
 
-module Map : Map.S with type key = t
+module Map : Map.S with type key = ex_purpose
 
-type operators = Signature.Public_key_hash.t Map.t
+(** Operator type. An operator is either a single key or a list of
+   keys. *)
+type 'a operator =
+  | Single : Signature.public_key_hash -> Signature.public_key_hash operator
+  | Multiple :
+      Signature.public_key_hash list
+      -> Signature.public_key_hash list operator
+
+(** existential type for operator. *)
+type ex_operator = Operator : 'a operator -> ex_operator
+
+(** [operators] type is a map from purpose to operator. This type is
+    private to control its creation and make sure [`kind t] and [`kind
+    operator] are correctly related with [`kind] *)
+type operators = private ex_operator Map.t
 
 type error +=
-  | Missing_operator of t
+  | Missing_operator of ex_purpose
   | Too_many_operators of {
-      expected_purposes : t list;
+      expected_purposes : ex_purpose list;
       given_operators : operators;
     }
 
 (** [to_string p] returns a string representation of purpose [p]. *)
-val to_string : t -> string
+val to_string : ex_purpose -> string
 
 (** [of_string s] parses a purpose from the given string [s]. *)
-val of_string : string -> t option
+val of_string : string -> ex_purpose option
 
 (** [of_string s] parses a purpose from the given string [s]. *)
-val of_string_exn : string -> t
+val of_string_exn : string -> ex_purpose
 
 val operators_encoding : operators Data_encoding.t
 
@@ -42,8 +64,8 @@ val operators_encoding : operators Data_encoding.t
     fails.*)
 val make_operator :
   ?default_operator:Signature.public_key_hash ->
-  needed_purposes:t list ->
-  (t * Signature.public_key_hash) list ->
+  needed_purposes:ex_purpose list ->
+  (ex_purpose * Signature.public_key_hash) list ->
   operators tzresult
 
 (** [replace_operator ?default_operator ~needed_purposes purposed_keys
@@ -54,15 +76,30 @@ val make_operator :
     contains only mapping of purpose from needed_purposes.*)
 val replace_operator :
   ?default_operator:Signature.public_key_hash ->
-  needed_purposes:t list ->
-  (t * Signature.public_key_hash) list ->
+  needed_purposes:ex_purpose list ->
+  (ex_purpose * Signature.public_key_hash) list ->
   operators ->
   operators tzresult
 
 (** For each purpose, it returns a list of associated operation
     kinds. *)
-val operation_kind : t -> Operation_kind.t list
+val operation_kind : ex_purpose -> Operation_kind.t list
 
 (** For a list of operation kind, it returns a list of associated
     purpose. *)
-val of_operation_kind : Operation_kind.t list -> t list
+val of_operation_kind : Operation_kind.t list -> ex_purpose list
+
+(** [find_operator purpose operators] returns the {!operator} for the
+    [purpose] from the [operators] list. The ['kind] type is to know
+    if this purpose allows only one or multiple operators. *)
+val find_operator : 'kind t -> operators -> 'kind operator option
+
+(** [mem_operator operator operators] checks of [operator] is part of
+    any purpose in [operators]. *)
+val mem_operator : Signature.public_key_hash -> operators -> bool
+
+(** function to bypass the private type. The opposite is not exposed
+    so we don't risk corrupting the map with invalid ['kind1
+    purpose_kind -> 'kind2 operator] mapping where ['kind1 <>
+    'kind2] *)
+val operators_bindings : operators -> (ex_purpose * ex_operator) list