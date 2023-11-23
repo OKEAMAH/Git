6788630bbca152db1c14f72f166aca29546673cf
Antonio Locascio <antonio.locascio@nomadic-labs.com>
2023-11-23 15:05:22 +0100

    EVM/Kernel: add roundtrip test for seq blueprint encoding

--- src/kernel_evm/kernel/src/sequencer_blueprint.rs
+++ src/kernel_evm/kernel/src/sequencer_blueprint.rs
@@ -4,8 +4,8 @@
 
 use crate::inbox::Transaction;
 use rlp::{Decodable, DecoderError, Encodable, Rlp};
+use tezos_ethereum::rlp_helpers::decode_list;
 use tezos_ethereum::rlp_helpers::{self, append_timestamp, decode_timestamp};
-use tezos_ethereum::rlp_helpers::{decode_list, FromRlpBytes};
 use tezos_smart_rollup_encoding::timestamp::Timestamp;
 use tezos_smart_rollup_host::runtime::Runtime;
 
@@ -57,5 +57,70 @@ pub fn fetch<Host: Runtime>(host: &mut Host) -> anyhow::Result<Vec<SequencerBlue
             Ok(seq_blueprints)
         }
         _ => Ok(vec![]),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SequencerBlueprint;
+    use crate::inbox::Transaction;
+    use crate::inbox::TransactionContent::Ethereum;
+    use primitive_types::{H160, U256};
+    use rlp::{encode_list, Rlp};
+    use tezos_ethereum::rlp_helpers::decode_list;
+    use tezos_ethereum::{
+        transaction::TRANSACTION_HASH_SIZE, tx_common::EthereumTransactionCommon,
+    };
+    use tezos_smart_rollup_encoding::timestamp::Timestamp;
+
+    fn sequencer_blueprints_roundtrip(v: Vec<SequencerBlueprint>) {
+        let bytes = encode_list(&v);
+        let rlp = Rlp::new(&bytes);
+        let v2: Vec<SequencerBlueprint> = decode_list(&rlp, "seq_blueprints")
+            .expect("Sequencer blueprints should be decodable");
+        assert_eq!(v, v2, "Roundtrip failed on {:?}", v)
+    }
+
+    fn address_from_str(s: &str) -> Option<H160> {
+        let data = &hex::decode(s).unwrap();
+        Some(H160::from_slice(data))
+    }
+
+    fn tx_(i: u64) -> EthereumTransactionCommon {
+        EthereumTransactionCommon {
+            type_: tezos_ethereum::transaction::TransactionType::Legacy,
+            chain_id: U256::one(),
+            nonce: U256::from(i),
+            max_priority_fee_per_gas: U256::from(40000000u64),
+            max_fee_per_gas: U256::from(40000000u64),
+            gas_limit: 21000u64,
+            to: address_from_str("423163e58aabec5daa3dd1130b759d24bef0f6ea"),
+            value: U256::from(500000000u64),
+            data: vec![],
+            access_list: vec![],
+            signature: None,
+        }
+    }
+
+    fn dummy_transaction(i: u8) -> Transaction {
+        Transaction {
+            tx_hash: [i; TRANSACTION_HASH_SIZE],
+            content: Ethereum(tx_(i.into())),
+        }
+    }
+
+    fn dummy_blueprint() -> SequencerBlueprint {
+        SequencerBlueprint {
+            timestamp: Timestamp::from(42),
+            transactions: vec![dummy_transaction(0), dummy_transaction(1)],
+        }
+    }
+
+    #[test]
+    fn roundtrip_rlp() {
+        {
+            let v = vec![dummy_blueprint(), dummy_blueprint()];
+            sequencer_blueprints_roundtrip(v);
+        }
     }
 }
