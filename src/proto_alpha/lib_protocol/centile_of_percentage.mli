b4e0b1d5dbb9b192554e051b6d0b97eb18a6982b
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-11-28 10:08:38 +0100

    Proto: add module Centile_of_percentage

---
+++ src/proto_alpha/lib_protocol/centile_of_percentage.mli
@@ -0,0 +1,9 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2023 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** A value between 0 and 100_00. *)
+type t = private Uint63.t
