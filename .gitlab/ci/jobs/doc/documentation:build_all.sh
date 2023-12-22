2d255a3d588855bf3aa54095a062950416ee710b
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-12-22 10:29:00 +0100

    Add timing everywhere

--- .gitlab/ci/jobs/doc/documentation:build_all.sh
+++ .gitlab/ci/jobs/doc/documentation:build_all.sh
@@ -3,6 +3,8 @@
 set -ex
 
 ./scripts/remove-old-protocols.sh .trash
-make all
+make all SHELL=$(pwd)/scripts/timing.sh
 ./scripts/restore-old-protocols.sh .trash
-make -C docs -j all
+make -C docs -j all SHELL=$(pwd)/scripts/timing.sh
+
+cat /tmp/make_log
