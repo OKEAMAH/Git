e302030f016019f58609a1ecd1919aec4c5d6437
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-01-16 11:45:22 +0100

    TMP empty take_ownership.sh

--- scripts/ci/take_ownership.sh
+++ scripts/ci/take_ownership.sh
@@ -4,7 +4,7 @@
 # Used to retake ownership of the checkout, to work around an issue in
 # GitLab CI runner.
 
-script_dir="$(cd "$(dirname "$0")" && echo "$(pwd -P)/")"
-src_dir="$(dirname "$(dirname "$script_dir")")"
+# script_dir="$(cd "$(dirname "$0")" && echo "$(pwd -P)/")"
+# src_dir="$(dirname "$(dirname "$script_dir")")"
 
-sudo chown -R "$(id -u)":"$(id -g)" "$src_dir"
+# sudo chown -R "$(id -u)":"$(id -g)" "$src_dir"
