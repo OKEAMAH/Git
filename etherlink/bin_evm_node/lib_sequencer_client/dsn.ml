80a60b7e35e371e572887303245c73c67f5115d3
Andrea Cerone <andrea.cerone@trili.tech>
2024-01-17 12:34:50 +0000

    Sequencer client: monitor preblocks endpoint for DSN

--- etherlink/bin_evm_node/lib_sequencer_client/dsn.ml
+++ etherlink/bin_evm_node/lib_sequencer_client/dsn.ml
@@ -15,5 +15,31 @@ let submit_transactions_rpc =
     ~service:"narwhal.Transactions"
     ~method_:"SubmitTransaction"
 
+let monitor_preblocks_rpc =
+  Grpc_client.Rpc.make
+    Exporter.Exporter.Exporter.export
+    ~service:"exporter.Exporter"
+    ~method_:"Export"
+
 let submit_transaction ?timeout client tx_data =
   Grpc_client.call ?timeout ~rpc:submit_transactions_rpc client tx_data
+
+let monitor_preblocks client ~since =
+  let open Lwt_result_syntax in
+  let* Grpc_client.{stream; _} =
+    Grpc_client.streamed_call client ~rpc:monitor_preblocks_rpc since
+  in
+  return
+  @@ Lwt_stream.map
+       (fun subdag ->
+         Result.map
+           (fun Exporter.Exporter.SubDag.{payloads; _} ->
+             let rev_txs =
+               List.fold_left
+                 (fun txs batches -> List.append (List.concat batches) txs)
+                 []
+                 payloads
+             in
+             List.rev rev_txs)
+           subdag)
+       stream
