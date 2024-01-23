8f3ddc72e374e73ef55c721cdeef4f1437dee9ae
Felix Puscasu <felix.puscasu@trili.tech>
2024-01-23 15:51:42 +0000

    RISC-V: I-ext: JALR

--- src/risc_v/machine_state/src/interpreter/rv32i.rs
+++ src/risc_v/machine_state/src/interpreter/rv32i.rs
@@ -4,7 +4,9 @@
 
 use crate::{
     backend,
+    bus::Address,
     registers::{XRegister, XRegisters},
+    HartState,
 };
 
 impl<M> XRegisters<M>
@@ -18,5 +20,44 @@ where
         let rval = self.read(rs1);
         let result = rval.wrapping_add(imm as u64);
         self.write(rd, result);
+    }
+}
+
+impl<M> HartState<M>
+where
+    M: backend::Manager,
+{
+    /// run "jalr" instruction
+    ///
+    /// Instruction mis-aligned will never be thrown because we allow C extension
+    ///
+    /// Returns the previous address before the target address jumped to (jump address - 4 bytes)
+    pub fn run_jalr(&mut self, imm: i64, rs1: XRegister, rd: XRegister) -> Address {
+        // Save the address after jump into rd
+        let after_jump_addr = self.pc.read().wrapping_add(4);
+
+        self.xregisters.write(rd, after_jump_addr);
+
+        // The target address is obtained by adding the sign-extended
+        // 12-bit I-immediate to the register rs1, then setting
+        // the least-significant bit of the result to zero
+        let target_addr = self.xregisters.read(rs1).wrapping_add(imm as u64) & !1;
+
+        target_addr.wrapping_sub(4)
+    }
+
+    /// run "jal" instruction
+    ///
+    /// Instruction mis-aligned will never be thrown because we allow C extension
+    ///
+    /// Returns the previous address before the target address (jump address - 4 bytes)
+    pub fn run_jal(&mut self, imm: i64, rd: XRegister) -> Address {
+        // Save the address after jump into rd
+        let current_pc = self.pc.read();
+
+        let after_jump_addr = current_pc.wrapping_add(4);
+        self.xregisters.write(rd, after_jump_addr);
+
+        current_pc.wrapping_add(imm as u64).wrapping_sub(4)
     }
 }
