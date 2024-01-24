4e76c539ca08d8ea866f792eb89b9a6bf5ea80d1
Anne-Laure <al.schmitt@protonmail.com>
2024-01-24 15:38:17 +0100

    Crypto_dal/Srs_verifier: add function to print verifier’s SRS

--- src/lib_crypto_dal/srs_verifier.ml
+++ src/lib_crypto_dal/srs_verifier.ml
@@ -92,3 +92,116 @@ let fake_srs =
   let srs_g1 = Srs_g1.generate_insecure length secret in
   let srs_g2 = Srs_g2.generate_insecure length secret in
   (srs_g1, srs_g2)
+
+(* Bounds (in log₂)
+   1 <= redundancy<= 4
+   7 <= page size + (redundancy + 1) <= slot size <= 20
+   5 <= page size <= slot size - (redundancy + 1) <= 18 - 5 = 13
+   2 <= redundancy + 1 <= nb shards <= slot size - page size <= 15
+   we call range the number of logs to go through
+   we call offset the index to start (included)
+*)
+type range = {
+  redundancy_range : int;
+  redundancy_offset : int;
+  slot_range : int;
+  slot_offset : int;
+  page_range : int;
+  page_offset : int;
+}
+
+let small_params_for_tests =
+  {
+    redundancy_range = 4;
+    redundancy_offset = 1;
+    slot_range = 6;
+    slot_offset = 8;
+    page_range = 3;
+    page_offset = 5;
+  }
+
+let generate_poly_lengths p =
+  let page_srs =
+    let values =
+      List.init p.page_range (fun i ->
+          domain_length ~size:(1 lsl (i + p.page_offset)))
+    in
+    values
+  in
+  let commitment_srs =
+    List.init p.slot_range (fun slot_size ->
+        let slot_size = slot_size + p.slot_offset in
+        List.init p.redundancy_range (fun redundancy ->
+            let redundancy = redundancy + p.redundancy_offset in
+            let page_range =
+              max 0 (slot_size - (redundancy + 1) - p.page_offset + 1)
+            in
+            List.init page_range (fun page_size ->
+                let page_size = page_size + p.page_offset in
+                Parameters_bounds_for_tests.max_srs_size
+                - slot_as_polynomial_length
+                    ~page_size:(1 lsl page_size)
+                    ~slot_size:(1 lsl slot_size))))
+    |> List.concat |> List.concat
+  in
+  let shard_srs =
+    List.init p.slot_range (fun slot_size ->
+        let slot_size = slot_size + p.slot_offset in
+        List.init p.redundancy_range (fun redundancy ->
+            let redundancy = redundancy + p.redundancy_offset in
+            let page_range =
+              max 0 (slot_size - (redundancy + 1) - p.page_offset + 1)
+            in
+            List.init page_range (fun page_size ->
+                let page_size = page_size + p.page_offset in
+                let shard_range = max 0 (slot_size - page_size + 1) in
+                let shard_offset = redundancy + 1 in
+                List.init shard_range (fun nb_shards ->
+                    let nb_shards = nb_shards + shard_offset in
+                    redundancy
+                    * slot_as_polynomial_length
+                        ~page_size:(1 lsl page_size)
+                        ~slot_size:(1 lsl slot_size)
+                    / nb_shards))))
+    |> List.concat |> List.concat |> List.concat
+  in
+  List.sort_uniq Int.compare (page_srs @ commitment_srs @ shard_srs)
+
+let print_verifier_srs () =
+  let srs_g1, srs_g2 = fake_srs in
+  let srs2 =
+    List.map
+      (fun i ->
+        let g2 =
+          Srs_g2.get srs_g2 i |> G2.to_compressed_bytes |> Hex.of_bytes
+          |> Hex.show
+        in
+        Printf.sprintf "(%d, \"%s\")" i g2)
+      (generate_poly_lengths small_params_for_tests)
+  in
+  let srs1 =
+    List.init (1 lsl 8) (fun i ->
+        Printf.sprintf
+          "\"%s\""
+          (Srs_g1.get srs_g1 i |> G1.to_compressed_bytes |> Hex.of_bytes
+         |> Hex.show))
+  in
+  Printf.printf
+    "\n\nlet srs_g1 = [|\n  %s\n|] |> read_srs_g1"
+    (String.concat " ;\n  " @@ srs1) ;
+  Printf.printf
+    "\n\nlet srs_g2 = [\n  %s\n] |> read_srs_g2"
+    (String.concat " ;\n  " @@ srs2)
+
+let read_srs_g1 srs1 =
+  let srs1 =
+    Array.map
+      (fun s -> Hex.to_bytes_exn (`Hex s) |> G1.of_compressed_bytes_exn)
+      srs1
+  in
+  Srs_g1.of_array srs1
+
+let read_srs_g2 srs2 =
+  List.map
+    (fun (i, s) -> (i, Hex.to_bytes_exn (`Hex s) |> G2.of_compressed_bytes_exn))
+    srs2
