c662cb1e26a7386e8583a6c68543c2ece9f241bc
Antonio Locascio <antonio.locascio@nomadic-labs.com>
2024-02-02 14:44:28 +0100

    EVM/Kernel: read delayed inbox min levels constant

--- etherlink/kernel_evm/kernel/src/storage.rs
+++ etherlink/kernel_evm/kernel/src/storage.rs
@@ -90,6 +90,11 @@ const TRANSACTIONS_INDEX: RefPath = RefPath::assert_from(b"/transactions");
 const EVM_DELAYED_INBOX_TIMEOUT: RefPath =
     RefPath::assert_from(b"/delayed_inbox_timeout");
 
+// Path to the number of l1 levels that need to pass for a
+// delayed tx to be timed out.
+const EVM_DELAYED_INBOX_MIN_LEVELS: RefPath =
+    RefPath::assert_from(b"/delayed_inbox_min_levels");
+
 /// The size of one 256 bit word. Size in bytes
 pub const WORD_SIZE: usize = 32usize;
 
@@ -849,6 +854,19 @@ pub fn delayed_inbox_timeout<Host: Runtime>(host: &Host) -> anyhow::Result<u64> 
             "Using default delayed inbox timeout of 12 hours"
         );
         Ok(default_timeout)
+    }
+}
+
+pub fn delayed_inbox_min_levels<Host: Runtime>(host: &Host) -> anyhow::Result<u32> {
+    // TODO: which value to use as default?
+    let default_min_levels = 720;
+    if host.store_has(&EVM_DELAYED_INBOX_MIN_LEVELS)?.is_some() {
+        let mut buffer = [0u8; 4];
+        store_read_slice(host, &EVM_DELAYED_INBOX_MIN_LEVELS, &mut buffer, 4)?;
+        Ok(u32::from_le_bytes(buffer))
+    } else {
+        log!(host, Info, "Using default delayed inbox minimum levels");
+        Ok(default_min_levels)
     }
 }
 
